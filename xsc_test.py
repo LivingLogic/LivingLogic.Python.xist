dd8cb3ae2b7ec0dcfd18b014bdd15a87a769010a
Walter Doerwald <walter@livinglogic.de>
1999-08-30 21:44:26 +0200

    Testskript an neue API angepasst.

--- xsc_test.py
+++ xsc_test.py
@@ -2,17 +2,18 @@
 
 from xsc_specials import *
 
+xsc = XSC()
 try:
-	a = plaintable(
+	xsc.root = plaintable(
 		img(src="hurz.gif",width="%(width)d*2",height=2)+
 		img(src="hurz.gif")+
 		filesize("hurz.gif")+
 		nbsp()
 	)
 	print "XML:"
-	print AsString(a)
+	print xsc.AsString()
 	print "HTML:"
-	print AsString(AsHTML(a))
+	print xsc.AsHTML().AsString()
 except EHSCException,e:
 	print str(e)
 
