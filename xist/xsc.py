65215cee3dfd56da62e372fc840b0e555d15a2e6
Walter Doerwald <walter@livinglogic.de>
1999-10-20 18:15:57 +0200

    Neue Funktionen URLForInput und URLForOutput, die die URL-Verwurstelung ?bernehmen. (noch nicht fertig)

--- xist/xsc.py
+++ xist/xsc.py
@@ -102,6 +102,26 @@ class XSCIllegalObjectError(XSCError):
 ###
 ###
 
+def URLForInput(url):
+	(scheme,server,path,parameters,query,fragment) = urlparse.urlparse(url)
+	if scheme == "" and server == "":
+		if len(path) and path[0] == "/": # this is a server relative URL, use the server specified in the options (usually localhost)
+			scheme = "http"
+			server = xsc.server
+		else:
+			if len(path) and path[0] == ":": # project relative, i.e. relative to the current directory
+				path = path[1:]
+			# now we have an URL that is relative to the current directory, replace URL syntax with the path syntax on our system (won't do anything under UNIX, replaces / with  \ under Windows)
+			pathsplit = string.splitfields(path,"/")
+			for i in range(len(pathsplit)):
+				if pathsplit[i] == "..":
+					pathsplit[i] = os.pardir
+			path = string.joinfields(pathsplit,os.sep)
+	return urlparse.urlunparse((scheme,server,path,parameters,query,fragment))
+
+def URLForOutput(url):
+	pass
+
 def FileSize(url):
 	"""returns the size of a file in bytes or -1 if the file shouldn't be read"""
 
