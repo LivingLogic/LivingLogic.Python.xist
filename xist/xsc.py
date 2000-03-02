99a7d6d767c7080d768438c4e9476d9c55223f3d
Walter Doerwald <walter@livinglogic.de>
2000-03-02 17:13:52 +0100

    Tippfehler in Copyright-Notiz.

--- xist/xsc.py
+++ xist/xsc.py
@@ -6,7 +6,7 @@
 ##
 ## Permission to use, copy, modify, and distribute this software and its documentation
 ## for any purpose and without fee is hereby granted, provided that the above copyright
-## notice appear in all copies and that both that copyright notice and this permission notice
+## notice appears in all copies and that both that copyright notice and this permission notice
 ## appear in supporting documentation, and that the name of Living Logic AG not be used
 ## in advertising or publicity pertaining to distribution of the software without specific,
 ## written prior permission.
