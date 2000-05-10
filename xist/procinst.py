ef853e1baa1058d1db2e9173d44c6ec9010513e8
Walter Doerwald <walter@livinglogic.de>
2000-05-10 17:31:42 +0200

    procinst-Doku ein bischen erweitert.

--- xist/procinst.py
+++ xist/procinst.py
@@ -2,7 +2,7 @@
 
 """
 This module provides a sandbox for the code that is evaluated in
-XSC processing instructions.
+XSC processing instructions (i.e. <?xsc-exec?> and <?xsc-eval?>).
 """
 
 __version__ = "$Revision$"[11:-2]
