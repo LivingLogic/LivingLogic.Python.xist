d66d6dba01afe49c35faef2a350c6e979b8d12cf
Walter Doerwald <walter@livinglogic.de>
2000-09-05 20:30:26 +0200

    Nun als normales CGI-Skript.

--- examples/apache-icons/index.py
+++ examples/apache-icons/index.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-import httpdapi
-
 from xist import xsc,html,specials
 
 import glob
@@ -9,40 +7,38 @@ import os
 
 cols = 6
 
-class RequestHandler(httpdapi.RequestHandler):
-	def Content(self):
-		xsc.xsc.pushURL("/icons/") # from now on (until popURL() is called) all URLs will be relative to /icons
+xsc.xsc.pushURL("/icons/") # from now on (until popURL() is called) all URLs will be relative to /icons
 
-		e = xsc.Frag()
+e = xsc.Frag()
 
+collect = xsc.Frag()
+i = 0
+
+files = glob.glob("*.gif")
+files.sort()
+
+for file in files:
+	collect.append(html.td(html.img(src = file),html.br(),file,align="center")) # src is relative to the top of the URL stack
+	i = i + 1
+	if i == cols:
+		e.append(html.tr(collect))
 		collect = xsc.Frag()
 		i = 0
+if len(collect):
+	e.append(html.tr(collect))
 
-		files = glob.glob("*.gif")
-		files.sort()
+e = html.html(
+	html.head(
+		html.title("All icons"),
+		html.link(rel="stylesheet",type="text/css",href="icons.css") # href is relative to the top of the URL stack
+	),
+	html.body(
+		specials.plaintable(e)
+	)
+)
 
-		for file in files:
-			collect.append(html.td(html.img(src = file),html.br(),file,align="center")) # src is relative to the top of the URL stack
-			i = i + 1
-			if i == cols:
-				e.append(html.tr(collect))
-				collect = xsc.Frag()
-				i = 0
-		if len(collect):
-			e.append(html.tr(collect))
+s = e.asHTML().asString(XHTML=0)
 
-		e = html.html(
-			html.head(
-				html.title("All icons"),
-				html.link(rel="stylesheet",type="text/css",href="icons.css") # href is relative to the top of the URL stack
-			),
-			html.body(
-				specials.plaintable(e)
-			)
-		)
+xsc.xsc.popURL() # don't forget to call popURL(), otherwise the URL stack will be messed up.
 
-		s = e.asHTML().asString(XHTML=0)
-
-		xsc.xsc.popURL() # don't forget to call popURL(), otherwise the URL stack will be messed up.
-
-		return s
+print s
