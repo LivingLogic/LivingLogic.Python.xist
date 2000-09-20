a3f7ec350a20c3d6f10e82f39d9fdfbb66a0dd71
Walter Doerwald <walter@livinglogic.de>
2000-09-20 17:06:04 +0200

    contenttype in neues Modul ausgelagert.

---
+++ xist/meta.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+"""
+A XSC module that contains elements that simplify handling
+meta data.
+"""
+
+__version__ = "$Revision$"[11:-2]
+# $Source$
+
+import xsc, html
+
+class contenttype(html_.meta):
+	empty = 1
+	attrHandlers = html.meta.attrHandlers.copy()
+	del attrHandlers["http-equiv"]
+	del attrHandlers["http_equiv"]
+	del attrHandlers["name"]
+	del attrHandlers["content"]
+
+	def asHTML(self):
+		e = html_.meta(self.attrs)
+		e["http-equiv"] = "Content-Type"
+		e["content"] = "text/html"
+
+		return e.asHTML()
+
+namespace = xsc.Namespace("meta", "http://www.livinglogic.de/DTDs/meta.dtd", vars())
+
