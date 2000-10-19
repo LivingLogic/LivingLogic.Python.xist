574a4ecafafd024997c7b3f1de4bed51a53f8b46
Walter Doerwald <walter@livinglogic.de>
2000-10-19 11:46:12 +0200

    Doku aktualisiert.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "$Revision$"[11:-2]
 from distutils.core import setup
 
 setup(name = "XIST",
-      version = "0.4.1",
+      version = "0.4.2",
       description = "An XML based extensible HTML generator",
       author = "Walter Dörwald",
       author_email = "walter@livinglogic.de",
