ffca5b6f1d06eb26e22c99ae21b3bf730acb032f
Walter Doerwald <walter@livinglogic.de>
2001-03-16 13:52:19 +0100

    Attribut type f?r Element embed.

--- xist/html.py
+++ xist/html.py
@@ -597,7 +597,7 @@ class embed(xsc.Element):
 	generic embedded object (Internet Exploder)
 	"""
 	empty = 0
-	attrHandlers = {"width": xsc.TextAttr, "height": xsc.TextAttr, "src": xsc.URLAttr, "controller": xsc.TextAttr, "href": xsc.URLAttr, "target": xsc.TextAttr, "border": xsc.IntAttr, "pluginspage": xsc.URLAttr, "quality": xsc.TextAttr}
+	attrHandlers = {"width": xsc.TextAttr, "height": xsc.TextAttr, "src": xsc.URLAttr, "controller": xsc.TextAttr, "href": xsc.URLAttr, "target": xsc.TextAttr, "border": xsc.IntAttr, "pluginspage": xsc.URLAttr, "quality": xsc.TextAttr, "type": xsc.TextAttr}
 
 class map(xsc.Element):
 	"""
