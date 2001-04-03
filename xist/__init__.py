166f36fe6eb121f67e0212dc18b9d085b2c277be
Walter Doerwald <walter@livinglogic.de>
2001-04-03 17:44:40 +0200

    Alte Files weg.

--- xist/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
-# $Source$
-
-__all__ = ["xsc", "publishers", "presenters", "parsers", "publishers", "ns"]
-
-
--- xist/procinst.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-"""
-This module provides a sandbox for the code that is evaluated in
-XSC processing instructions (i.e. <?xsc:exec?> and <?xsc:eval?>).
-"""
-
-__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
-# $Source$
