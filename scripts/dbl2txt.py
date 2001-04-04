9fcd342aabed490e0ddc9575325100acd0bf36dc
Walter Doerwald <walter@livinglogic.de>
2001-04-04 11:50:20 +0200

    Skripten in scripts geschoben.

---
+++ scripts/dbl2txt.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+## Copyright 1999-2001 by LivingLogic AG, Bayreuth, Germany.
+## Copyright 1999-2001 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## Permission to use, copy, modify, and distribute this software and its documentation
+## for any purpose and without fee is hereby granted, provided that the above copyright
+## notice appears in all copies and that both that copyright notice and this permission
+## notice appear in supporting documentation, and that the name of LivingLogic AG or
+## the author not be used in advertising or publicity pertaining to distribution of the
+## software without specific, written prior permission.
+##
+## LIVING LOGIC AG AND THE AUTHOR DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+## INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+## LIVING LOGIC AG OR THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+## DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+## IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+## IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+"""
+Module that uses the w3m browser to generate a text version
+of a docbook fragment.
+Usage: python docbooklite2text.py spam.xml spam.txt
+       to generate spam.txt from spam.xml
+"""
+
+__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
+# $Source$
+
+import sys, os, getopt
+
+from xist import xsc, parsers
+from xist.ns import html, docbooklite as dbl
+
+def xsc2txt(infilename, outfilename, title):
+	e = parsers.parseFile(infilename, namespaces=xsc.Namespaces(dbl))
+
+	if title is None:
+		title = xsc.Null
+	else:
+		title = dbl.title(title)
+	e = html.html(
+		html.body(
+			dbl.section(title, e)
+		)
+	)
+
+	e = e.convert()
+
+	(stdin, stdout) = os.popen2("w3m -T text/html -dump >%s" % outfilename)
+
+	stdin.write(e.asBytes())
+	stdin.close()
+	stdout.close()
+
+if __name__ == "__main__":
+	title = None
+	(options, args) = getopt.getopt(sys.argv[1:], "t:i:", ["title=", "import="])
+
+	for (option, value) in options:
+		if option=="-t" or option=="--title":
+			title = value
+		elif option=="-i" or option=="--import":
+			__import__(value)
+
+	xsc2txt(args[0], args[1], title)
---
+++ scripts/xscmake.py
@@ -0,0 +1,134 @@
+#! /usr/bin/env python
+
+## Copyright 1999-2001 by LivingLogic AG, Bayreuth, Germany.
+## Copyright 1999-2001 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## Permission to use, copy, modify, and distribute this software and its documentation
+## for any purpose and without fee is hereby granted, provided that the above copyright
+## notice appears in all copies and that both that copyright notice and this permission
+## notice appear in supporting documentation, and that the name of LivingLogic AG or
+## the author not be used in advertising or publicity pertaining to distribution of the
+## software without specific, written prior permission.
+##
+## LIVINGLOGIC AG AND THE AUTHOR DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+## INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+## LIVINGLOGIC AG OR THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+## DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+## IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+## IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+"""
+This modules contains stuff to be able to use XIST from
+the command line as a compiler.
+"""
+
+__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
+# $Source$
+
+import sys
+import getopt
+import time
+
+import ansistyle
+
+from xist import xsc, publishers, presenters, url, utils, converters, parsers # don't do a subpackage import here, otherwise chaos will ensue, because XIST modules will be imported twice
+
+def mapExt(ext):
+	try:
+		return {
+			"hsc": "html",
+			"shsc": "shtml",
+			"phsc": "phtml",
+			"xsc": "html",
+			"sxsc": "shtml",
+			"pxsc": "phtml",
+			"jxsc": "jsp",
+			"jxscp": "jspp"
+		}[ext]
+	except KeyError:
+		return ext
+
+def make(args):
+	"""
+	use XSC as a compiler script
+	"""
+	(options, args) = getopt.getopt(args, "p:i:o:e:x:m:f:r:n:", ["path=", "import=", "output=", "encoding=", "xhtml=", "mode=", "files=", "parser=", "namespace="])
+
+	globaloutname = url.URL("*/")
+	encoding = None
+	XHTML = None
+	mode = None
+	filesname = None
+	parsername = "sgmlop"
+	namespaces = xsc.Namespaces()
+
+	for (option, value) in options:
+		if option=="-p" or option=="--path":
+			sys.path.append(value)
+		elif option=="-i" or option=="--import":
+			__import__(value)
+		elif option=="-o" or option=="--output":
+			globaloutname = url.URL(value)
+		elif option=="-e" or option=="--encoding":
+			encoding = value
+		elif option=="-x" or option=="--xhtml":
+			XHTML = int(value)
+		elif option=="-m" or option=="--mode":
+			mode = value
+		elif option=="-f" or option=="--files":
+			filesname = value
+		elif option=="-r" or option=="--parser":
+			parsername = value
+		elif option=="-n" or option=="--namespace":
+			namespaces.pushNamespace(xsc.namespaceRegistry.byPrefix[value.strip()])
+
+	files = []
+	if filesname is not None:
+		for filename in open(filesname,"r").readlines():
+			filename = filename.strip()
+			if filename != "":
+				files.append(filename)
+	files.extend(args)
+
+	if files:
+		converter = converters.Converter(mode)
+		for file in files:
+			if parsername=="sgmlop":
+				parser = parsers.SGMLOPParser()
+			elif parsername=="expat":
+				parser = parsers.ExpatParser()
+			else:
+				raise ValueError("parser must be 'sgmlop' or 'expat', but not %r" % parsername)
+			inname = url.URL(file)
+			outname = globaloutname.clone()
+			if not outname.file:
+				outname += inname
+			if not outname.file:
+				outname.file = "noname"
+			try:
+				outname.ext = mapExt(inname.ext)
+			except KeyError:
+				outname.ext = "html"
+			t1 = time.clock()
+			e_in = parsers.parseFile(inname.asString(), namespaces=namespaces)
+			t2 = time.clock()
+			e_out = e_in.convert(converter)
+			t3 = time.clock()
+			p = publishers.FilePublisher(utils.forceopen(outname.asPlainString(), "wb", 65536), base=outname, encoding=encoding, XHTML=XHTML)
+			e_out.publish(p)
+			t4 = time.clock()
+			size = p.tell()
+			sys.stderr.write(
+				"XSC(encoding=%s; XHTML=%s; parse %ss; convert %ss; save %ss; size %s bytes): %s->%s\n" %
+				(presenters.strString(p.encoding), presenters.strNumber(p.XHTML),
+				 presenters.strNumber("%.02f" % (t2-t1)), presenters.strNumber("%.02f" % (t3-t2)), presenters.strNumber("%.02f" % (t4-t3)),
+				 presenters.strNumber(size),
+				 presenters.strURL(inname), presenters.strURL(outname))
+			)
+	else:
+		sys.stderr.write("XSC: no files to convert.\n")
+
+if __name__ == "__main__":
+	make(sys.argv[1:])
