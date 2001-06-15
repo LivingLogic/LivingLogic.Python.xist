ad69a42f55acd4e30a0e1e6b6844052b54465c72
Walter Doerwald <walter@livinglogic.de>
2001-06-15 13:54:51 +0200

    Doku in HOWTO.xml ausgelagert.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 
 setup(
 	name = "XIST",
-	version = "1.0a1",
+	version = "1.0",
 	description = "An XML based extensible HTML generator",
 	author = "Walter Dörwald",
 	author_email = "walter@livinglogic.de",
