d5db2fed488e05ce926595447a86bf33d069b4a5
Walter Doerwald <walter@livinglogic.de>
2001-06-18 14:22:30 +0200

    "tree"!

--- _xist/__init__.py
+++ _xist/__init__.py
@@ -3,7 +3,7 @@
 """
 <dbl:para>&xist; is an &xml; based extensible &html; generator written in Python.
 &xist; is also a &dom; parser (built on top of &sax;2) with a very simple and
-pythonesque &api;. Every &xml; element type corresponds to a Python class and these
+pythonesque tree &api;. Every &xml; element type corresponds to a Python class and these
 Python classes provide a conversion method to transform the &xml; tree (e.g. into
 &html;). &xist; can be considered <z>object oriented &xsl;</z>.</dbl:para>
 
