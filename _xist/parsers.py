47cb85fade44209ed512cec73a1ce1bb79544a87
Walter Doerwald <walter@livinglogic.de>
2001-06-21 13:16:22 +0200

    Kein root: etc. beim URL ?ffnen.

--- _xist/parsers.py
+++ _xist/parsers.py
@@ -65,7 +65,7 @@ class URLInputSource(sax.xmlreader.InputSource):
 	def __init__(self, url, defaultEncoding="utf-8"):
 		sax.xmlreader.InputSource.__init__(self)
 		if isinstance(url, url_.URL):
-			url = url.asString()
+			url = url.asPlainString()
 		self.setSystemId(url)
 		if type(url) is types.UnicodeType:
 			url = url.encode("utf-8")
