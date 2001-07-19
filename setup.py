28786d59cfff44f4fca369932922566be58968e9
Walter Doerwald <walter@livinglogic.de>
2001-07-19 13:59:24 +0200

    Prepare for release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 
 setup(
 	name = "XIST",
-	version = "1.1a2",
+	version = "1.1",
 	description = "An XML-based extensible HTML generator",
 	author = "Walter Dörwald",
 	author_email = "walter@livinglogic.de",
