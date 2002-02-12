ca1b40f02b84a3a83e6c47a8dccb7eac7a187f33
Walter Doerwald <walter@livinglogic.de>
2002-02-12 11:34:05 +0100

    Added a few attributes to xist.ns.html.applet.

--- _xist/ns/html.py
+++ _xist/ns/html.py
@@ -791,7 +791,8 @@ class applet(xsc.Element): # deprecated
 	Java applet
 	"""
 	empty = 0
-	attrHandlers = {"code": xsc.TextAttr, "codebase": xsc.URLAttr, "object": xsc.TextAttr, "width": xsc.TextAttr, "height": xsc.TextAttr}
+	attrHandlers = coreattrs.copy()
+	attrHandlers.update({"name": xsc.TextAttr, "code": xsc.TextAttr, "codebase": xsc.URLAttr, "object": xsc.TextAttr, "width": xsc.TextAttr, "height": xsc.TextAttr})
 	attrHandlers.update({"archive": xsc.TextAttr}) # deprecated
 
 class nobr(xsc.Element): # deprecated
