a11b0b44e722077cf0906f12daf080cbdd7bb9d5
Walter Doerwald <walter@livinglogic.de>
2002-10-15 11:04:27 +0200

    Use the correct package name.

--- _xist/publishers.py
+++ _xist/publishers.py
@@ -160,7 +160,7 @@ class Publisher(object):
 				needed = self._neededxmlnsdefs(self.node)
 				if needed:
 					if len(needed)>1 or not isinstance(needed[0], xsc.Element):
-						from xist.ns import specials
+						from ll.xist.ns import specials
 						self.node = specials.wrap(self.node)
 
 			prefixes2use = {}
