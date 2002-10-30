6698f85d9c6826d23a10a8497aae31befdadfa7f
Walter Doerwald <walter@livinglogic.de>
2002-10-30 17:56:21 +0100

    Wrap the description in the setup script.
    Add news about the latest changes.
    
    Prepare for a new release.

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ from distutils.core import setup, Extension
 
 setup(
 	name="XIST",
-	version="2.0.2",
+	version="2.0.3",
 	description="An XML-based extensible HTML generator",
-	long_description="XIST is an XML based extensible HTML generator. XIST is also a DOM parser (built on top of SAX2)\n"
-		"with a very simple and pythonesque tree API. Every XML element type corresponds to a Python class and these\n"
-		"Python classes provide a conversion method to transform the XML tree (e.g. into HTML).\n"
+	long_description="XIST is an XML based extensible HTML generator. XIST is also\n"
+		"a DOM parser (built on top of SAX2) with a very simple and pythonesque tree API.\n"
+		"Every XML element type corresponds to a Python class and these Python classes\n"
+		"provide a conversion method to transform the XML tree (e.g. into HTML).\n"
 		"XIST can be considered 'object oriented XSL'.",
 	author="Walter Dörwald",
 	author_email="walter@livinglogic.de",
