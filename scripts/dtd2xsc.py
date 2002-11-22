08105454aced73d5d71e6ff731f8c35be9053c9e
Walter Doerwald <walter@livinglogic.de>
2002-11-22 19:02:34 +0100

    Updated the script to use the functionality of the new xndl namespace module.

--- scripts/dtd2xsc.py
+++ scripts/dtd2xsc.py
@@ -27,37 +27,18 @@ Usage: python dtd2xsc.py foo.dtd
        to generate foo.py
 """
 
-__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
+__version__ = "$Revision$"[11:-2]
 # $Source$
 
-import sys, keyword, os.path
+import sys, os.path
 
 from xml.parsers.xmlproc import dtdparser
 
+import optik
+
 from ll import url
 from ll.xist import xsc, parsers
-
-def pyify(name):
-	if keyword.iskeyword(name):
-		return name + "_"
-	else:
-		newname = []
-		for c in name:
-			if "a" <= c <= "z" or "A" <= c <= "Z" or "0" <= c <= "9" or c == "_":
-				newname.append(c)
-			else:
-				newname.append("_")
-		return "".join(newname)
-
-def simplify(value):
-	try:
-		value = int(value)
-	except ValueError:
-		try:
-			value = str(value)
-		except UnicodeError:
-			pass
-	return value
+from ll.xist.ns import xndl
 
 def dtd2xsc(dtdfilename, outfilename=None):
 	# get name of dtd without extension
@@ -70,85 +51,57 @@ def dtd2xsc(dtdfilename, outfilename=None):
 
 	# write header
 	file = open(outfilename, 'w')
-	file.write('#! /usr/bin/env python\n\n')
-	file.write('"""\n')
-	file.write('"""\n\n')
-	file.write('__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))\n')
-	file.write('# $Source$\n\n')
-	file.write('from ll.xist import xsc\n\n')
 
+	e = xndl.xndl(prefix=modname, name="... insert namespace URI ...")
+	
 	# write elements
 	elements = dtd.get_elements()
 	elements.sort()
 	for elemname in elements:
-		pyelemname = pyify(elemname)
-		file.write('class %s(xsc.Element):\n\t"""\n\t"""\n' % (pyelemname))
-		if pyelemname != elemname:
-			file.write('\txmlname = "%s"\n' % elemname)
 		elem = dtd.get_elem(elemname)
 		if elem.get_content_model() == ("", [], ""):
-			empty = "True"
+			empty = True
 		else:
-			empty = "False"
+			empty = None
+		e.append(xndl.element(name=elemname, empty=empty))
 
-		# write empty and attribute class
-		file.write('\tempty = %s\n' % empty)
 		attrs = elem.get_attr_list()
 		if len(attrs):
 			attrs.sort()
-			file.write('\tclass Attrs(xsc.Element.Attrs):\n')
 			for attrname in attrs:
 				if u":" in attrname:
 					continue # skip global attributes
-				pyattrname = pyify(attrname) # get a version of the name that can be used as a Python identifier.
 				attr = elem.get_attr(attrname)
-				values = None
+				values = []
 				if attr.type == "ID":
 					type = "IDAttr"
 				else:
 					type = "TextAttr"
 					if isinstance(attr.type, list):
 						if len(attr.type)>1:
-							values = repr(tuple([ simplify(value) for value in attr.type]))
+							values = attr.type
 						else:
 							type = "BoolAttr"
 				if attr.default is not None:
-					default = repr(simplify(attr.default))
+					default = attr.default
 				else:
 					default = None
 				if attr.decl=="#REQUIRED":
 					required = True
 				else:
 					required = None
-				file.write('\t\tclass %s(xsc.%s):' % (pyattrname, type))
-				if values is not None or default is not None or required is not None:
-					if pyattrname != attrname:
-						file.write('\n\t\t\txmlname = "%s"' % attrname)
-					if values is not None:
-						file.write('\n\t\t\tvalues = %s' % values)
-					if default is not None:
-						file.write('\n\t\t\tdefault = %s' % default)
-					if required is not None:
-						file.write('\n\t\t\trequired = True')
-				else:
-					if pyattrname != attrname:
-						file.write(' xmlname = "%s"' % attrname)
-					else:
-						file.write(' pass')
-				file.write('\n')
-		file.write('\n')
+				e[-1].append(xndl.attr(name=attrname, type=type, default=default, required=required))
+				for value in values:
+					e[-1][-1].append(xndl.value(value))
 
 	# write entities
 	ents = dtd.get_general_entities()
+	ents.sort()
 	for entname in ents:
-		if entname not in ('quot', 'apos', 'gt', 'lt', 'amp'):
+		if entname not in ("quot", "apos", "gt", "lt", "amp"):
 			ent = parsers.parseString(dtd.resolve_ge(entname).value)
-			file.write('class %s(xsc.CharRef): " "; codepoint = %d\n' % (entname, ord(unicode(ent[0])[0])))
-	file.write('\n')
-
-	# write namespace registration
-	file.write("# register all the classes we've defined so far\n")
-	file.write('xmlns = xsc.Namespace("%s", "... insert namespace URI ...", vars())' % (modname))
+			e.append(xndl.charref(name=entname, codepoint=ord(unicode(ent[0])[0])))
+	file.write(e.aspy())
 	file.close()
 
 if __name__ == "__main__":
