8a0da7ba515a2bc3dab229a2ca10c8501f42cb1c
Walter Doerwald <walter@livinglogic.de>
2003-05-29 14:42:14 +0200

    Add namespaces for Zopes TAL and METAL attribute languages (closes issue18)

---
+++ _xist/ns/metal.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2003 by LivingLogic AG, Bayreuth, Germany.
+## Copyright 1999-2003 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+"""
+<par>An &xist; module that contains the global attributes from
+<app>Zope</app>s <z>Macro Expansion Template Attribute Language</z>.</par>
+"""
+
+__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
+# $Source$
+
+from ll.xist import xsc
+
+class xmlns(xsc.Namespace):
+	xmlname = "metal"
+	xmlurl = "http://xml.zope.org/namespaces/metal"
+
+	class Attrs(xsc.Namespace.Attrs):
+		class define_macro(xsc.TextAttr): xmlname = "define-macro"
+		class use_macro(xsc.TextAttr): xmlname = "use-macro"
+		class define_slot(xsc.TextAttr): xmlname = "define-slot"
+		class use_slot(xsc.TextAttr): xmlname = "use-slot"
+
+xmlns.makemod(vars())
---
+++ _xist/ns/tal.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2003 by LivingLogic AG, Bayreuth, Germany.
+## Copyright 1999-2003 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+"""
+<par>An &xist; module that contains the global attributes from
+<app>Zope</app>s <z>Template Attribute Language</z>.</par>
+"""
+
+__version__ = tuple(map(int, "$Revision$"[11:-2].split(".")))
+# $Source$
+
+from ll.xist import xsc
+
+class xmlns(xsc.Namespace):
+	xmlname = "tal"
+	xmlurl = "http://xml.zope.org/namespaces/tal"
+
+	class Attrs(xsc.Namespace.Attrs):
+		class define(xsc.TextAttr): pass
+		class attributes(xsc.TextAttr): pass
+		class condition(xsc.TextAttr): pass
+		class replace(xsc.TextAttr): pass
+		class repeat(xsc.TextAttr): pass
+		class on_error(xsc.TextAttr): xmlname = "on-error"
+		class omit_tag(xsc.TextAttr): xmlname = "omit-tag"
+
+xmlns.makemod(vars())
