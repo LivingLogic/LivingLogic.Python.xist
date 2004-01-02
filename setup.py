dbb24a3db079ec9c3990ed75af965ef17bfaf41a
Walter Doerwald <walter@livinglogic.de>
2004-01-02 17:34:30 +0100

    Update version number.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ DESCRIPTION = "\n".join(textwrap.wrap(DESCRIPTION.strip(), width=64, replace_whi
 
 setup(
 	name="ll-xist",
-	version="2.3",
+	version="2.4",
 	description="An XML-based extensible HTML generator",
 	long_description=DESCRIPTION,
 	author="Walter Doerwald",
