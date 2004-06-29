fa07072e02f2ce49d1050d7d53142263e486027c
Walter Doerwald <walter@livinglogic.de>
2004-06-29 16:04:01 +0200

    Regenerate from DTD with sims info.

--- _xist/ns/struts_config.py
+++ _xist/ns/struts_config.py
@@ -35,97 +35,8 @@ class ElementWithID(xsc.Element):
 		class id(xsc.IDAttr): pass
 
 
-class struts_config(ElementWithID):
-	xmlname = "struts-config"
-	empty = False
-
-
-class data_sources(ElementWithID):
-	xmlname = "data-sources"
-	empty = False
-
-
-class data_source(ElementWithID):
-	xmlname = "data-source"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class className(xsc.TextAttr): pass
-		class key(xsc.TextAttr): pass
-		class type(xsc.TextAttr): pass
-
-
-class form_beans(ElementWithID):
-	xmlname = "form-beans"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class type(xsc.TextAttr): pass
-
-
-class form_bean(ElementWithID):
-	xmlname = "form-bean"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class className(xsc.TextAttr): pass
-		class dynamic(xsc.TextAttr): values = ("true", "false", "yes", "no")
-		class name(xsc.TextAttr): required = True
-		class type(xsc.TextAttr): required = True
-
-
-class form_property(xsc.Element):
-	xmlname = "form-property"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class className(xsc.TextAttr): pass
-		class initial(xsc.TextAttr): pass
-		class name(xsc.TextAttr): required = True
-		class size(xsc.TextAttr): pass
-		class type(xsc.TextAttr): required = True
-
-
-class global_exceptions(ElementWithID):
-	xmlname = "global-exceptions"
-	empty = False
-
-
-class exception(ElementWithID):
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class bundle(xsc.TextAttr): pass
-		class className(xsc.TextAttr): pass
-		class handler(xsc.TextAttr): pass
-		class key(xsc.TextAttr): required = True
-		class path(xsc.TextAttr): pass
-		class scope(xsc.TextAttr): pass
-		class type(xsc.TextAttr): required = True
-
-
-class global_forwards(ElementWithID):
-	xmlname = "global-forwards"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class type(xsc.TextAttr): pass
-
-
-class forward(ElementWithID):
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class className(xsc.TextAttr): pass
-		class contextRelative(xsc.TextAttr): values = ("true", "false", "yes", "no")
-		class name(xsc.TextAttr): required = True
-		class path(xsc.TextAttr): required = True
-		class redirect(xsc.TextAttr): values = ("true", "false", "yes", "no")
-
-
-class action_mappings(ElementWithID):
-	xmlname = "action-mappings"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
-		class type(xsc.TextAttr): pass
-
-
 class action(ElementWithID):
-	empty = False
-	class Attrs(xsc.Element.Attrs):
+	class Attrs(ElementWithID.Attrs):
 		class attribute(xsc.TextAttr): pass
 		class className(xsc.TextAttr): pass
 		class forward(xsc.TextAttr): pass
@@ -143,9 +54,14 @@ class action(ElementWithID):
 		class validate(xsc.TextAttr): values = ("true", "false", "yes", "no")
 
 
+class action_mappings(ElementWithID):
+	xmlname = "action-mappings"
+	class Attrs(ElementWithID.Attrs):
+		class type(xsc.TextAttr): pass
+
+
 class controller(ElementWithID):
-	empty = False
-	class Attrs(xsc.Element.Attrs):
+	class Attrs(ElementWithID.Attrs):
 		class bufferSize(xsc.TextAttr): pass
 		class className(xsc.TextAttr): pass
 		class contentType(xsc.TextAttr): pass
@@ -162,10 +78,92 @@ class controller(ElementWithID):
 		class tempDir(xsc.TextAttr): pass
 
 
+class data_source(ElementWithID):
+	xmlname = "data-source"
+	class Attrs(ElementWithID.Attrs):
+		class className(xsc.TextAttr): pass
+		class key(xsc.TextAttr): pass
+		class type(xsc.TextAttr): pass
+
+
+class data_sources(ElementWithID):
+	xmlname = "data-sources"
+
+
+class description(ElementWithID):
+	pass
+
+
+class display_name(ElementWithID):
+	xmlname = "display-name"
+
+
+class exception(ElementWithID):
+	class Attrs(ElementWithID.Attrs):
+		class bundle(xsc.TextAttr): pass
+		class className(xsc.TextAttr): pass
+		class handler(xsc.TextAttr): pass
+		class key(xsc.TextAttr): required = True
+		class path(xsc.TextAttr): pass
+		class scope(xsc.TextAttr): pass
+		class type(xsc.TextAttr): required = True
+
+
+class form_bean(ElementWithID):
+	xmlname = "form-bean"
+	class Attrs(ElementWithID.Attrs):
+		class className(xsc.TextAttr): pass
+		class dynamic(xsc.TextAttr): values = ("true", "false", "yes", "no")
+		class name(xsc.TextAttr): required = True
+		class type(xsc.TextAttr): required = True
+
+
+class form_beans(ElementWithID):
+	xmlname = "form-beans"
+	class Attrs(ElementWithID.Attrs):
+		class type(xsc.TextAttr): pass
+
+
+class form_property(xsc.Element):
+	xmlname = "form-property"
+	class Attrs(xsc.Element.Attrs):
+		class className(xsc.TextAttr): pass
+		class initial(xsc.TextAttr): pass
+		class name(xsc.TextAttr): required = True
+		class size(xsc.TextAttr): pass
+		class type(xsc.TextAttr): required = True
+
+
+class forward(ElementWithID):
+	class Attrs(ElementWithID.Attrs):
+		class className(xsc.TextAttr): pass
+		class contextRelative(xsc.TextAttr): values = ("true", "false", "yes", "no")
+		class name(xsc.TextAttr): required = True
+		class path(xsc.TextAttr): required = True
+		class redirect(xsc.TextAttr): values = ("true", "false", "yes", "no")
+
+
+class global_exceptions(ElementWithID):
+	xmlname = "global-exceptions"
+
+
+class global_forwards(ElementWithID):
+	xmlname = "global-forwards"
+	class Attrs(ElementWithID.Attrs):
+		class type(xsc.TextAttr): pass
+
+
+class icon(ElementWithID):
+	pass
+
+
+class large_icon(ElementWithID):
+	xmlname = "large-icon"
+
+
 class message_resources(ElementWithID):
 	xmlname = "message-resources"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
+	class Attrs(ElementWithID.Attrs):
 		class className(xsc.TextAttr): pass
 		class factory(xsc.TextAttr): pass
 		class key(xsc.TextAttr): pass
@@ -175,47 +173,53 @@ class message_resources(ElementWithID):
 
 class plug_in(ElementWithID):
 	xmlname = "plug-in"
-	empty = False
-	class Attrs(xsc.Element.Attrs):
+	class Attrs(ElementWithID.Attrs):
 		class className(xsc.TextAttr): required = True
-
-
-class description(ElementWithID):
-	empty = False
-
-
-class display_name(ElementWithID):
-	xmlname = "display-name"
-	empty = False
-
-
-class icon(ElementWithID):
-	empty = False
-
-
-class large_icon(ElementWithID):
-	xmlname = "large-icon"
-	empty = False
 
 
 class set_property(ElementWithID):
 	xmlname = "set-property"
-	empty = True
-	class Attrs(xsc.Element.Attrs):
+	class Attrs(ElementWithID.Attrs):
 		class property(xsc.TextAttr): required = True
 		class value(xsc.TextAttr): required = True
 
 
 class small_icon(ElementWithID):
 	xmlname = "small-icon"
-	empty = False
+
+
+class struts_config(ElementWithID):
+	xmlname = "struts-config"
+
+
+action_mappings.model = sims.Elements(action)
+data_sources.model = sims.Elements(data_source)
+exception.model = \
+forward.model = sims.Elements(display_name, description, set_property, icon)
+global_exceptions.model = sims.Elements(exception)
+action.model = sims.Elements(exception, description, forward, display_name, set_property, icon)
+form_beans.model = sims.Elements(form_bean)
+form_bean.model = sims.Elements(form_property, display_name, description, set_property, icon)
+global_forwards.model = sims.Elements(forward)
+struts_config.model = sims.Elements(global_exceptions, controller, message_resources, data_sources, plug_in, action_mappings, form_beans, global_forwards)
+controller.model = \
+data_source.model = \
+form_property.model = \
+message_resources.model = \
+plug_in.model = sims.Elements(set_property)
+icon.model = sims.Elements(small_icon, large_icon)
+set_property.model = sims.Empty()
+description.model = \
+display_name.model = \
+large_icon.model = \
+small_icon.model = sims.NoElements()
 
 
 # this is no "official" struts-config element, but nonetheless useful for generating
 # the final XML output
 class user_struts_config(xsc.Element):
-	empty = False
 	xmlname = "user-struts-config"
+	model = struts_config.model
 
 	def convert(self, converter):
 		e = xsc.Frag(
