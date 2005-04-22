e868917ec12b5f9c7c5e2ebb354995248734db1f
Walter Doerwald <walter@livinglogic.de>
2005-04-22 19:22:13 +0200

    Use xfind.count and [] for counting root nodes.

--- _xist/publishers.py
+++ _xist/publishers.py
@@ -20,7 +20,7 @@ import sys, codecs
 
 from ll import url
 
-import xsc, options, helpers
+import xsc, options, helpers, xfind
 
 
 def cssescapereplace(exc):
@@ -173,7 +173,7 @@ class Publisher(object):
 				prefixes2def[child.__ns__] = True
 
 		# Determine if we have multiple roots
-		if prefixes2def and isinstance(node, xsc.Frag) and len(node.find(xsc.FindType(xsc.Element))) > 1:
+		if prefixes2def and isinstance(node, xsc.Frag) and xfind.count(node[xsc.Element]) > 1:
 			raise xsc.MultipleRootsError()
 
 		if prefixes2def:
