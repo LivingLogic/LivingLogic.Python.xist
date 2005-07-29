493b7c3ace5148ee54c5ff4bfc5c800872ac1662
Walter Doerwald <walter@livinglogic.de>
2005-07-29 17:45:09 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ DESCRIPTION = "\n".join(textwrap.wrap(DESCRIPTION.strip(), width=64, replace_whi
 
 setup(
 	name="ll-xist",
-	version="2.10",
+	version="2.11",
 	description="An extensible HTML/XML generator",
 	long_description=DESCRIPTION,
 	author=u"Walter Dörwald",
