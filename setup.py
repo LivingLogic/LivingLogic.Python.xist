20bfad0642d4300477b5f333dc02f21e69a8549b
Walter Doerwald <walter@livinglogic.de>
2005-10-31 14:14:46 +0100

    Let tokenize() work with unicode directly and remove the encoding argument.
    asString() now honors the encoding argument insofar as characters not
    encodable in this encoding will be output as character references. The
    return value is still a unicode string.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ DESCRIPTION = "\n".join(textwrap.wrap(DESCRIPTION.strip(), width=64, replace_whi
 
 setup(
 	name="ll-xist",
-	version="2.12",
+	version="2.13",
 	description="An extensible HTML/XML generator",
 	long_description=DESCRIPTION,
 	author=u"Walter Dörwald",
