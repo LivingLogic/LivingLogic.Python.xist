4ce43d9c9b3ff1e6757f0451bc7814e467a59df5
Walter Doerwald <walter@livinglogic.de>
2006-08-09 14:04:59 +0200

    Add missing __init__ file for the script package.

---
+++ src/ll/xist/scripts/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2006 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 1999-2006 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+__version__ = "$Revision$".split()[1]
+# $Source$
+
