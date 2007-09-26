bb72bd3168e2fb0130b0b0cec2381e6eeeecd2df
Walter Doerwald <walter@livinglogic.de>
2007-09-26 13:35:54 +0200

    Fix tld2xsc, sot that it uses stdin/stdout instead of filenames on the command line.

--- src/ll/xist/scripts/tld2xsc.py
+++ src/ll/xist/scripts/tld2xsc.py
@@ -26,51 +26,30 @@ from ll.xist import xsc, xfind, parsers, converters
 from ll.xist.ns import tld
 
 
-def tld2xsc(inurl, outurl, verbose, xmlname, xmlurl, shareattrs):
-	if verbose:
-		print "Parsing TLD %s ..." % dtdurl
-	node = parsers.parseURL(inurl)
-
-	if verbose:
-		print "Converting ..."
+def tld2xnd(stream, shareattrs):
+	node = parsers.parsestream(stream)
 
 	# get and convert the taglib object
-	node = xfind.first(node/tld.taglib)
-	data = node.asxnd()
+	xnd = node.walknode(tld.taglib)[0].asxnd()
 
 	if shareattrs=="dupes":
-		data.shareattrs(False)
+		xnd.shareattrs(False)
 	elif shareattrs=="all":
-		data.shareattrs(True)
-
-	if verbose:
-		print "Writing to %s ..." % outurl
-
-	file = outurl.openwrite()
-	file.write(data.aspy())
-	file.close()
+		xnd.shareattrs(True)
+	return xnd
 
 
 def main():
-	p = optparse.OptionParser(usage="usage: %prog [options] inputurl.tld")
-	p.add_option("-o", "--output", dest="output", metavar="FILE", help="write output to FILE")
-	p.add_option("-v", "--verbose", action="store_true", dest="verbose")
-	p.add_option("-p", "--prefix", dest="xmlname", help="the XML prefix for this namespace", default="prefix", metavar="PREFIX")
-	p.add_option("-u", "--url", dest="xmlurl", help="the XML namespace name", metavar="URL")
+	p = optparse.OptionParser(usage="usage: %prog [options] <input.tld >output_xmlns.py")
 	p.add_option("-s", "--shareattrs", dest="shareattrs", help="Should identical attributes be shared among elements?", choices=("none", "dupes", "all"), default="dupes")
 	p.add_option("-m", "--model", dest="model", default="once", help="Add sims information to the namespace", choices=("no", "all", "once"))
 	p.add_option("-d", "--defaults", action="store_true", dest="defaults", help="Output default values for attributes")
 
 	(options, args) = p.parse_args()
-	if len(args) != 1:
+	if len(args) != 0:
 		p.error("incorrect number of arguments")
 		return 1
-	input = url.URL(args[0])
-	if options.output is None:
-		output = url.File(input.withExt("py").file)
-	else:
-		output = url.URL(options.output)
-	tld2xsc(input, output, options.verbose, options.xmlname, options.xmlurl, options.shareattrs, options.model, options.defaults)
+	print tld2xnd(sys.stdin, options.shareattrs).aspy(model=options.model, defaults=options.defaults)
 
 
 if __name__ == "__main__":
