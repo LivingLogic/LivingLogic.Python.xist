69be7b3251ddf441642af9d7fe4b13d55e450f83
Walter Doerwald <walter@livinglogic.de>
2007-12-25 16:35:05 +0100

    Update the media demo.

--- demos/media/Media.py
+++ demos/media/Media.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import with_statement
+
 from ll.xist import xsc, sims, parsers
 from ll.xist.ns import html, htmlspecials, meta, xml, chars
 
 
-class xmlns(xsc.Namespace):
-	xmlname = "media"
-	xmlurl = "http://xmlns.livinglogic.de/xist/demo/media"
+xmlns = "http://xmlns.livinglogic.de/xist/demo/media"
 
 
 class name(xsc.Element):
@@ -57,7 +57,7 @@ class price(xsc.Element):
 		class currency(xsc.TextAttr): pass
 
 	def convert(self, converter):
-		return xsc.Frag(self.content, " ", self["currency"]).convert(converter)
+		return xsc.Frag(self.content, " ", self.attrs.currency).convert(converter)
 
 
 class purchase(xsc.Element):
@@ -122,32 +122,28 @@ class media(xsc.Element):
 		dvds = xsc.Frag(self[dvd]).sorted(key=namekey)
 		lds = xsc.Frag(self[ld]).sorted(key=namekey)
 
-		e = xsc.Frag(
-			xml.XML(), "\n",
-			html.DocTypeXHTML10transitional(), "\n",
-			html.html(
-				html.head(
-					meta.contenttype(),
-					html.title("Media"),
-					meta.stylesheet(href="Media.css")
-				),
-				htmlspecials.plainbody(
-					html.h1("Media")
-				)
-			)
-		)
-		if lds:
-			e[-1][-1].append(html.h2(len(lds), " LDs"), html.ol(lds))
-		if dvds:
-			e[-1][-1].append(html.h2(len(dvds), " DVDs"), html.ol(dvds))
+		with xsc.Frag() as e:
+			+xml.XML()
+			+html.DocTypeXHTML10transitional()
+			with html.html():
+				with html.head():
+					+meta.contenttype()
+					+html.title("Media")
+					+meta.stylesheet(href="Media.css")
+				with htmlspecials.plainbody():
+					+html.h1("Media")
+					if lds:
+						+html.h2(len(lds), " LDs")
+						+html.ol(lds)
+					if dvds:
+						+html.h2(len(dvds), " DVDs")
+						+html.ol(dvds)
 		return e.convert(converter)
 
 
-
-
 if __name__ == "__main__":
-	prefixes = xsc.Prefixes([xmlns, chars.xmlns], xml=xml)
-	node = parsers.parseFile("Media.xml", prefixes=prefixes)
+	pool = xsc.Pool(vars(), chars, xml)
+	node = parsers.parsefile("Media.xml", pool=pool)
 	node = node[media][0]
 	node = node.conv()
 	print node.bytes(encoding="us-ascii")
