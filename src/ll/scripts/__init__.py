caccffa0e431d70b4439529bc196851d4712e71b
Walter Doerwald <walter@livinglogic.de>
2008-02-01 13:51:09 +0100

    Add missing package __init__.py.

---
+++ src/ll/scripts/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+## Copyright 1999-2008 by LivingLogic AG, Bayreuth/Germany
+## Copyright 1999-2008 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
