efd42048bd02b2ee5ee6ba954e038121a972e120
Walter Doerwald <walter@livinglogic.de>
2008-05-14 22:00:34 +0200

    More tests.

--- test/test_l4.py
+++ test/test_l4.py
@@ -24,8 +24,53 @@ def test_text():
 	yield check, u'g\xfcrk', {}, u'g\xfcrk'
 
 
-def test_print():
-	yield check, u'<?print "foo"?>', {}, u'foo'
-	yield check, u'<?print "\u20ac"?>', {}, u'\u20ac'
-	yield check, u'<?print "\\xff"?>', {}, u'\xff'
-	yield check, u'<?print "\\u20ac"?>', {}, u'\u20ac'
+def test_none():
+	yield check, '<?print None?>', {}, ''
+	yield check, '<?if None?>yes<?else?>no<?end if?>', {}, 'no'
+
+
+def test_false():
+	yield check, '<?print False?>', {}, 'False'
+	yield check, '<?if False?>yes<?else?>no<?end if?>', {}, 'no'
+
+
+def test_true():
+	yield check, '<?print True?>', {}, 'True'
+	yield check, '<?if True?>yes<?else?>no<?end if?>', {}, 'yes'
+
+
+def test_int():
+	yield check, '<?print 0?>', {}, '0'
+	yield check, '<?print 42?>', {}, '42'
+	yield check, '<?print -42?>', {}, '-42'
+	yield check, '<?print 0xff?>', {}, '255'
+	yield check, '<?print 0Xff?>', {}, '255'
+	yield check, '<?print -0xff?>', {}, '-255'
+	yield check, '<?print -0Xff?>', {}, '-255'
+	yield check, '<?print 0o77?>', {}, '63'
+	yield check, '<?print 0O77?>', {}, '63'
+	yield check, '<?print -0o77?>', {}, '-63'
+	yield check, '<?print -0O77?>', {}, '-63'
+	yield check, '<?print 0b111?>', {}, '7'
+	yield check, '<?print 0B111?>', {}, '7'
+	yield check, '<?print -0b111?>', {}, '-7'
+	yield check, '<?print -0B111?>', {}, '-7'
+	yield check, '<?if 0?>yes<?else?>no<?end if?>', {}, 'no'
+	yield check, '<?if 1?>yes<?else?>no<?end if?>', {}, 'yes'
+	yield check, '<?if -1?>yes<?else?>no<?end if?>', {}, 'yes'
+
+
+def test_string():
+	yield check, u'''<?print "foo"?>''', {}, u'foo'
+	yield check, u'''<?print "\\n"?>''', {}, u'\n'
+	yield check, u'''<?print "\\r"?>''', {}, u'\r'
+	yield check, u'''<?print "\\t"?>''', {}, u'\t'
+	yield check, u'''<?print "\\f"?>''', {}, u'\f'
+	yield check, u'''<?print "\\b"?>''', {}, u'\b'
+	yield check, u'''<?print "\\a"?>''', {}, u'\a'
+	yield check, u'''<?print "\\e"?>''', {}, u'\x1b'
+	yield check, u'''<?print "\u20ac"?>''', {}, u'\u20ac'
+	yield check, u'''<?print "\\xff"?>''', {}, u'\xff'
+	yield check, u'''<?print "\\u20ac"?>''', {}, u'\u20ac'
+	yield check, '<?if ""?>yes<?else?>no<?end if?>', {}, 'no'
+	yield check, '<?if "foo"?>yes<?else?>no<?end if?>', {}, 'yes'
