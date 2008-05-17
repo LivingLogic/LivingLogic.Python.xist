afae9be249365505a0f579319aff7074c0304b63
Walter Doerwald <walter@livinglogic.de>
2008-05-17 23:49:59 +0200

    Reorder arguments to the check functions.
    This makes it possible to make the data object and the templates dictionary optional.

--- test/test_l4.py
+++ test/test_l4.py
@@ -16,24 +16,24 @@ import py.test
 from ll import l4c
 
 
-def check(source, data, result):
+def check(result, source, data={}, templates={}):
 	# Check with template compiled from source
 	t1 = l4c.compile(source)
-	assert t1.renders(data) == result
+	assert t1.renders(data, templates) == result
 
 	# Check with template loaded again via the string interface
 	t2 = l4c.loads(t1.dumps())
-	assert t2.renders(data) == result
+	assert t2.renders(data, templates) == result
 
 	# Check with template loaded again via the stream interface
 	stream = cStringIO.StringIO()
 	t1.dump(stream)
 	stream.seek(0)
 	t3 = l4c.load(stream)
-	assert t3.renders(data) == result
+	assert t3.renders(data, templates) == result
 
 
-def checkcompileerror(source, data, msg):
+def checkcompileerror(msg, source):
 	try:
 		l4c.compile(source)
 	except Exception, exc:
@@ -42,11 +42,11 @@ def checkcompileerror(source, data, msg):
 		py.test.fail("Didn't raise exception")
 
 
-def checkrunerror(source, data, msg):
+def checkrunerror(msg, source, data={}, templates={}):
 	# Check with template compiled from source
 	t1 = l4c.compile(source)
 	try:
-		t1.renders(data)
+		t1.renders(data, templates)
 	except Exception, exc:
 		assert msg in str(exc)
 	else:
@@ -55,7 +55,7 @@ def checkrunerror(source, data, msg):
 	# Check with template loaded again via the string interface
 	t2 = l4c.loads(t1.dumps())
 	try:
-		t2.renders(data)
+		t2.renders(data, templates)
 	except Exception, exc:
 		assert msg in str(exc)
 	else:
@@ -67,7 +67,7 @@ def checkrunerror(source, data, msg):
 	stream.seek(0)
 	t3 = l4c.load(stream)
 	try:
-		t3.renders(data)
+		t3.renders(data, templates)
 	except Exception, exc:
 		assert msg in str(exc)
 	else:
@@ -75,170 +75,171 @@ def checkrunerror(source, data, msg):
 
 
 def test_text():
-	yield check, 'gurk', {}, 'gurk'
-	yield check, u'gurk', {}, u'gurk'
-	yield check, u'g\xfcrk', {}, u'g\xfcrk'
+	yield check, 'gurk', 'gurk'
+	yield check, u'gurk', u'gurk'
+	yield check, u'g\xfcrk', u'g\xfcrk'
 
 
 def test_none():
-	yield check, '<?print None?>', {}, ''
+	yield check, '', '<?print None?>'
 
-	yield check, '<?if None?>yes<?else?>no<?end if?>', {}, 'no'
+	yield check, 'no', '<?if None?>yes<?else?>no<?end if?>'
 
 
 def test_false():
-	yield check, '<?print False?>', {}, 'False'
+	yield check, 'False', '<?print False?>'
 
-	yield check, '<?if False?>yes<?else?>no<?end if?>', {}, 'no'
+	yield check, 'no', '<?if False?>yes<?else?>no<?end if?>'
 
 
 def test_true():
-	yield check, '<?print True?>', {}, 'True'
+	yield check, 'True', '<?print True?>'
 
-	yield check, '<?if True?>yes<?else?>no<?end if?>', {}, 'yes'
+	yield check, 'yes', '<?if True?>yes<?else?>no<?end if?>'
 
 
 def test_int():
-	yield check, '<?print 0?>', {}, '0'
-	yield check, '<?print 42?>', {}, '42'
-	yield check, '<?print -42?>', {}, '-42'
-	yield check, '<?print 0xff?>', {}, '255'
-	yield check, '<?print 0Xff?>', {}, '255'
-	yield check, '<?print -0xff?>', {}, '-255'
-	yield check, '<?print -0Xff?>', {}, '-255'
-	yield check, '<?print 0o77?>', {}, '63'
-	yield check, '<?print 0O77?>', {}, '63'
-	yield check, '<?print -0o77?>', {}, '-63'
-	yield check, '<?print -0O77?>', {}, '-63'
-	yield check, '<?print 0b111?>', {}, '7'
-	yield check, '<?print 0B111?>', {}, '7'
-	yield check, '<?print -0b111?>', {}, '-7'
-	yield check, '<?print -0B111?>', {}, '-7'
+	yield check, '0', '<?print 0?>'
+	yield check, '42', '<?print 42?>'
+	yield check, '-42', '<?print -42?>'
+	yield check, '255', '<?print 0xff?>'
+	yield check, '255', '<?print 0Xff?>'
+	yield check, '-255', '<?print -0xff?>'
+	yield check, '-255', '<?print -0Xff?>'
+	yield check, '63', '<?print 0o77?>'
+	yield check, '63', '<?print 0O77?>'
+	yield check, '-63', '<?print -0o77?>'
+	yield check, '-63', '<?print -0O77?>'
+	yield check, '7', '<?print 0b111?>'
+	yield check, '7', '<?print 0B111?>'
+	yield check, '-7', '<?print -0b111?>'
+	yield check, '-7', '<?print -0B111?>'
 
-	yield check, '<?if 0?>yes<?else?>no<?end if?>', {}, 'no'
-	yield check, '<?if 1?>yes<?else?>no<?end if?>', {}, 'yes'
-	yield check, '<?if -1?>yes<?else?>no<?end if?>', {}, 'yes'
+	yield check, 'no', '<?if 0?>yes<?else?>no<?end if?>'
+	yield check, 'yes', '<?if 1?>yes<?else?>no<?end if?>'
+	yield check, 'yes', '<?if -1?>yes<?else?>no<?end if?>'
 
 
 def test_float():
-	yield check, '<?print 0.?>', {}, '0.0'
-	yield check, '<?print 42.?>', {}, '42.0'
-	yield check, '<?print -42.?>', {}, '-42.0'
-	yield check, '<?print 1E42?>', {}, '1e+42'
-	yield check, '<?print 1e42?>', {}, '1e+42'
-	yield check, '<?print -1E42?>', {}, '-1e+42'
-	yield check, '<?print -1e42?>', {}, '-1e+42'
+	yield check, '0.0', '<?print 0.?>'
+	yield check, '42.0', '<?print 42.?>'
+	yield check, '-42.0', '<?print -42.?>'
+	yield check, '1e+42', '<?print 1E42?>'
+	yield check, '1e+42', '<?print 1e42?>'
+	yield check, '-1e+42', '<?print -1E42?>'
+	yield check, '-1e+42', '<?print -1e42?>'
 
-	yield check, '<?if 0.?>yes<?else?>no<?end if?>', {}, 'no'
-	yield check, '<?if 1.?>yes<?else?>no<?end if?>', {}, 'yes'
-	yield check, '<?if -1.?>yes<?else?>no<?end if?>', {}, 'yes'
+	yield check, 'no', '<?if 0.?>yes<?else?>no<?end if?>'
+	yield check, 'yes', '<?if 1.?>yes<?else?>no<?end if?>'
+	yield check, 'yes', '<?if -1.?>yes<?else?>no<?end if?>'
 
 
 def test_string():
-	yield check, u'''<?print "foo"?>''', {}, u'foo'
-	yield check, u'''<?print "\\n"?>''', {}, u'\n'
-	yield check, u'''<?print "\\r"?>''', {}, u'\r'
-	yield check, u'''<?print "\\t"?>''', {}, u'\t'
-	yield check, u'''<?print "\\f"?>''', {}, u'\f'
-	yield check, u'''<?print "\\b"?>''', {}, u'\b'
-	yield check, u'''<?print "\\a"?>''', {}, u'\a'
-	yield check, u'''<?print "\\e"?>''', {}, u'\x1b'
-	yield check, u'''<?print "\\""?>''', {}, u'"'
-	yield check, u'''<?print "\\'"?>''', {}, u"'"
-	yield check, u'''<?print "\u20ac"?>''', {}, u'\u20ac'
-	yield check, u'''<?print "\\xff"?>''', {}, u'\xff'
-	yield check, u'''<?print "\\u20ac"?>''', {}, u'\u20ac'
-	yield checkcompileerror, u'''<?print "?>''', {}, "Unterminated string"
-	yield check, u'''<?print "\\xxx"?>''', {}, "\\xxx"
-	yield check, u'''<?print "a\nb"?>''', {}, "a\nb"
+	yield check, 'foo', '<?print "foo"?>'
+	yield check, '\n', '<?print "\\n"?>'
+	yield check, '\r', '<?print "\\r"?>'
+	yield check, '\t', '<?print "\\t"?>'
+	yield check, '\f', '<?print "\\f"?>'
+	yield check, '\b', '<?print "\\b"?>'
+	yield check, '\a', '<?print "\\a"?>'
+	yield check, '\x1b', '<?print "\\e"?>'
+	yield check, '"', '<?print "\\""?>'
+	yield check, "'", '<?print "\\\'"?>'
+	yield check, u'\u20ac', u'<?print "\u20ac"?>'
+	yield check, u'\xff', u'<?print "\\xff"?>'
+	yield check, u'\u20ac', u'''<?print "\\u20ac"?>'''
+	yield checkcompileerror, "Unterminated string", '<?print "?>'
+	yield check, "\\xxx", '<?print "\\xxx"?>'
+	yield check, "a\nb", '<?print "a\nb"?>'
 
-	yield check, '<?if ""?>yes<?else?>no<?end if?>', {}, 'no'
-	yield check, '<?if "foo"?>yes<?else?>no<?end if?>', {}, 'yes'
+	yield check, 'no', '<?if ""?>yes<?else?>no<?end if?>'
+	yield check, 'yes', '<?if "foo"?>yes<?else?>no<?end if?>'
 
 
 def test_code_storevar():
-	yield check, u'''<?code x = 42?><?print x?>''', {}, u'42'
-	yield check, u'''<?code x = "xyzzy"?><?print x?>''', {}, u'xyzzy'
+	yield check, '42', '<?code x = 42?><?print x?>'
+	yield check, 'xyzzy', '<?code x = "xyzzy"?><?print x?>'
 
 
 def test_code_addvar():
-	yield check, u'''<?code x = 17?><?code x += 23?><?print x?>''', {}, u'40'
-	yield check, u'''<?code x = "xyz"?><?code x += "zy"?><?print x?>''', {}, u'xyzzy'
+	yield check, '40', '<?code x = 17?><?code x += 23?><?print x?>'
+	yield check, 'xyzzy', '<?code x = "xyz"?><?code x += "zy"?><?print x?>'
 
 
 def test_code_subvar():
-	yield check, u'''<?code x = 17?><?code x -= 23?><?print x?>''', {}, u'-6'
+	yield check, '-6', '<?code x = 17?><?code x -= 23?><?print x?>'
 
 
 def test_code_mulvar():
-	yield check, u'''<?code x = 17?><?code x *= 23?><?print x?>''', {}, u'391'
-	yield check, u'''<?code x = 17?><?code x *= "xyzzy"?><?print x?>''', {}, 17*u'xyzzy'
-	yield check, u'''<?code x = "xyzzy"?><?code x *= 17?><?print x?>''', {}, 17*u'xyzzy'
+	yield check, '391', '<?code x = 17?><?code x *= 23?><?print x?>'
+	yield check, 17*'xyzzy', '<?code x = 17?><?code x *= "xyzzy"?><?print x?>'
+	yield check, 17*'xyzzy', '<?code x = "xyzzy"?><?code x *= 17?><?print x?>'
 
 
 def test_code_floordivvar():
-	yield check, u'''<?code x = 5?><?code x //= 2?><?print x?>''', {}, u'2'
-	yield check, u'''<?code x = -5?><?code x //= 2?><?print x?>''', {}, u'-3'
+	yield check, '2', '<?code x = 5?><?code x //= 2?><?print x?>'
+	yield check, '-3', '<?code x = -5?><?code x //= 2?><?print x?>'
 
 
 def test_code_truedivvar():
-	yield check, u'''<?code x = 5?><?code x /= 2?><?print x?>''', {}, u'2.5'
-	yield check, u'''<?code x = -5?><?code x /= 2?><?print x?>''', {}, u'-2.5'
+	yield check, '2.5', '<?code x = 5?><?code x /= 2?><?print x?>'
+	yield check, '-2.5', '<?code x = -5?><?code x /= 2?><?print x?>'
 
 
 def test_code_modvar():
-	yield check, u'''<?code x = 1729?><?code x %= 23?><?print x?>''', {}, u'4'
+	yield check, '4', '<?code x = 1729?><?code x %= 23?><?print x?>'
 
 
 def test_code_delvar():
-	yield checkrunerror, u'''<?code x = 1729?><?code del x?><?print x?>''', {}, u'KeyError'
+	yield checkrunerror, 'KeyError', '<?code x = 1729?><?code del x?><?print x?>'
 
 
 def test_for_string():
-	yield check, u'''<?for c in data?>(<?print c?>)<?end for?>''', "", u''
-	yield check, u'''<?for c in data?>(<?print c?>)<?end for?>''', "gurk", u'(g)(u)(r)(k)'
+	yield check, '', '<?for c in data?>(<?print c?>)<?end for?>', ""
+	yield check, '(g)(u)(r)(k)', '<?for c in data?>(<?print c?>)<?end for?>', "gurk"
 
 
 def test_for_list():
-	yield check, u'''<?for c in data?>(<?print c?>)<?end for?>''', "", u''
-	yield check, u'''<?for c in data?>(<?print c?>)<?end for?>''', "gurk", u'(g)(u)(r)(k)'
+	yield check, '', '<?for c in data?>(<?print c?>)<?end for?>', ""
+	yield check, '(g)(u)(r)(k)', '<?for c in data?>(<?print c?>)<?end for?>', ["g", "u", "r", "k"]
 
 
 def test_for_dict():
-	yield check, u'''<?for c in data?>(<?print c?>)<?end for?>''', {}, u''
-	yield check, u'''<?for c in sorted(data)?>(<?print c?>)<?end for?>''', dict(a=1, b=2, c=3), u'(a)(b)(c)'
+	yield check, '', '<?for c in data?>(<?print c?>)<?end for?>', {}
+	yield check, '(a)(b)(c)', '<?for c in sorted(data)?>(<?print c?>)<?end for?>', dict(a=1, b=2, c=3)
 
 
 def test_for_nested():
-	yield check, u'''<?for list in data?>[<?for n in list?>(<?print n?>)<?end for?>]<?end for?>''', [[1, 2, 3], [4, 5, 6], [7, 8, 9]], u'[(1)(2)(3)][(4)(5)(6)][(7)(8)(9)]'
+	yield check, '[(1)(2)][(3)(4)]', '<?for list in data?>[<?for n in list?>(<?print n?>)<?end for?>]<?end for?>', [[1, 2], [3, 4]]
 
 
 def test_if():
-	yield check, u'''<?if data?><?print data?><?end if?>''', 42, u'42'
+	yield check, '42', '<?if data?><?print data?><?end if?>', 42
 
 
 def test_else():
-	yield check, u'''<?if data?><?print data?><?else?>no<?end if?>''', 42, u'42'
-	yield check, u'''<?if data?><?print data?><?else?>no<?end if?>''', 0, u'no'
+	yield check, '42', '<?if data?><?print data?><?else?>no<?end if?>', 42
+	yield check, 'no', '<?if data?><?print data?><?else?>no<?end if?>', 0
 
 
 def test_block_errors():
-	yield checkcompileerror, u'''<?for x in data?>''', {}, "unclosed blocks"
-	yield checkcompileerror, u'''<?for x in data?><?end if?>''', {}, "endif doesn't match any if"
-	yield checkcompileerror, u'''<?end?>''', {}, "not in any block"
-	yield checkcompileerror, u'''<?end for?>''', {}, "not in any block"
-	yield checkcompileerror, u'''<?end if?>''', {}, "not in any block"
-	yield checkcompileerror, u'''<?else?>''', {}, "else doesn't match any if"
-	yield checkcompileerror, u'''<?if data?>''', {}, "unclosed blocks"
-	yield checkcompileerror, u'''<?if data?><?else?>''', {}, "unclosed blocks"
-	yield checkcompileerror, u'''<?if data?><?else?><?else?>''', {}, "duplicate else"
-	yield checkcompileerror, u'''<?if data?><?else?><?elif data?>''', {}, "else already seen"
-	yield checkcompileerror, u'''<?if data?><?elif data?><?elif data?><?else?><?elif data?>''', {}, "else already seen"
+	yield checkcompileerror, "unclosed blocks", '<?for x in data?>'
+	yield checkcompileerror, "endif doesn't match any if", '<?for x in data?><?end if?>'
+	yield checkcompileerror, "not in any block", '<?end?>'
+	yield checkcompileerror, "not in any block", '<?end for?>'
+	yield checkcompileerror, "not in any block", '<?end if?>'
+	yield checkcompileerror, "else doesn't match any if", '<?else?>'
+	yield checkcompileerror, "unclosed blocks", '<?if data?>'
+	yield checkcompileerror, "unclosed blocks", '<?if data?><?else?>'
+	yield checkcompileerror, "duplicate else", '<?if data?><?else?><?else?>'
+	yield checkcompileerror, "else already seen", '<?if data?><?else?><?elif data?>'
+	yield checkcompileerror, "else already seen", '<?if data?><?elif data?><?elif data?><?else?><?elif data?>'
 
 
 def test_empty():
-	yield checkcompileerror, u'''<?print?>''', {}, "expression required"
-	yield checkcompileerror, u'''<?if?>''', {}, "expression required"
-	yield checkcompileerror, u'''<<?if x?><?elif?><?end if?>''', {}, "expression required"
-	yield checkcompileerror, u'''<?for?>''', {}, "loop expression required"
-	yield checkcompileerror, u'''<?code?>''', {}, "statement required"
+	yield checkcompileerror, "expression required", '<?print?>'
+	yield checkcompileerror, "expression required", '<?if?>'
+	yield checkcompileerror, "expression required", '<<?if x?><?elif?><?end if?>'
+	yield checkcompileerror, "loop expression required", '<?for?>'
+	yield checkcompileerror, "statement required", '<?code?>'
+	yield checkcompileerror, "render statement required", '<?render?>'
