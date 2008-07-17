ffd52f2ae27bf49f715d69e72a2dd9820360e52c
Walter Doerwald <walter@livinglogic.de>
2008-07-17 15:25:49 +0200

    Bump version number of UL4 compiled format.

--- src/ll/ul4c.py
+++ src/ll/ul4c.py
@@ -606,7 +606,7 @@ class Template(object):
 	Rendering the template can be done with the methods :meth:`render` (which
 	returns a generator) or :meth:`renders` (which returns a string).
 	"""
-	version = "4"
+	version = "5"
 
 	def __init__(self):
 		self.startdelim = None
