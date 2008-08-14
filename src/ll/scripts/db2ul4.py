3b39848b334d685d2f0578110002f8be15634bd6
Walter Doerwald <walter@livinglogic.de>
2008-08-14 22:20:01 +0200

    Add a top level dict-like object that connects to the db. Add System object.

--- src/ll/scripts/db2ul4.py
+++ src/ll/scripts/db2ul4.py
@@ -6,6 +6,11 @@ import sys, os, optparse, codecs
 from ll import ul4c
 
 
+class System(object):
+	def __getitem__(self, cmd):
+		return os.popen(cmd).read()
+
+
 class QueryList(object):
 	def __init__(self, connection):
 		self.connection = connection
@@ -26,59 +31,71 @@ class QueryIter(object):
 		return c
 
 
-def oracle(connectstring):
-	"""
-	Return an Oracle connection for the connectstring :var:`connectstring`.
-	"""
-	from ll import orasql
-	return orasql.connect(connectstring, readlobs=True)
+class DB(object):
+	def __getitem__(self, key):
+		if key == "iter":
+			return QueryIter(self.connection)
+		elif key == "list":
+			return QueryList(self.connection)
+		else:
+			raise KeyError(key)
 
 
-def sqlite(connectstring):
-	"""
-	Return a SQLite connection for the connectstring :var:`connectstring`.
-	"""
-	import sqlite3
-	db = sqlite3.connect(connectstring)
-	class Row(sqlite3.Row):
-		def __getitem__(self, key):
-			if isinstance(key, unicode):
-				key = key.encode("ascii")
-			return sqlite3.Row.__getitem__(self, key)
-	db.row_factory = Row
-	return db
+class Oracle(DB):
+	def __init__(self, connectstring):
+		from ll import orasql
+		self.connection = orasql.connect(connectstring, readlobs=True)
 
 
-def mysql(connectstring):
-	"""
-	Return a MySQL connection for the connectstring :var:`connectstring`.
-	"""
-	import MySQLdb
-	from MySQLdb import cursors
-	(user, host) = connectstring.split("@")
-	(user, passwd) = user.split("/")
-	(host, db) = host.split("/")
-	db = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db, use_unicode=True, cursorclass=cursors.DictCursor)
-	return db
+class SQLite(DB):
+	def __init__(self, connectstring):
+		import sqlite3
+		connection = sqlite3.connect(connectstring)
+		class Row(sqlite3.Row):
+			def __getitem__(self, key):
+				if isinstance(key, unicode):
+					key = key.encode("ascii")
+				return sqlite3.Row.__getitem__(self, key)
+		connection.row_factory = Row
+		self.connection = connection
+
+
+class MySQL(DB):
+	def __init__(self, connectstring):
+		import MySQLdb
+		from MySQLdb import cursors
+		(user, host) = connectstring.split("@")
+		(user, passwd) = user.split("/")
+		(host, db) = host.split("/")
+		self.connection = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db, use_unicode=True, cursorclass=cursors.DictCursor)
+
+
+class Connect(object):
+	def __getitem__(self, connectstring):
+		(type, sep, connectstring) = connectstring.partition(":")
+		if type == "oracle":
+			return Oracle(connectstring)
+		elif type == "sqlite":
+			return SQLite(connectstring)
+		elif type == "mysql":
+			return SQLite(connectstring)
+		else:
+			return KeyError(connectstring)
 
 
 def main(args=None):
-	p = optparse.OptionParser(usage="usage: %prog [options] connectstring maintemplate [subtemplate1 subtemplate2 ...]")
-	dbs = dict(oracle=oracle, sqlite=sqlite, mysql=mysql)
-	p.add_option("-d", "--database", dest="database", help="Database type (%s)" % ", ".join(dbs), choices=dbs.keys(), default="sqlite")
+	p = optparse.OptionParser(usage="usage: %prog [options] maintemplate [subtemplate1 subtemplate2 ...]")
 	p.add_option("-i", "--inputencoding", dest="inputencoding", help="Encoding for template sources", default="utf-8", metavar="ENCODING")
 	p.add_option("-o", "--outputencoding", dest="outputencoding", help="Encoding for output", default="utf-8", metavar="ENCODING")
 
 	(options, args) = p.parse_args(args)
-	if len(args) < 2:
+	if len(args) < 1:
 		p.error("incorrect number of arguments")
 		return 1
 
-	db = dbs[options.database](args[0])
-
 	templates = {}
 	maintemplate = None
-	for templatename in args[1:]:
+	for templatename in args:
 		if templatename == "-":
 			templatestream = sys.stdin
 			templatename = "stdin"
@@ -92,7 +109,7 @@ def main(args=None):
 			maintemplate = template
 		templates[templatename] = template
 
-	vars = dict(iters=QueryIter(db), lists=QueryList(db), encoding=options.outputencoding, templates=templates)
+	vars = dict(connect=Connect(), system=System(), encoding=options.outputencoding, templates=templates)
 	for part in codecs.iterencode(maintemplate.render(**vars), options.outputencoding):
 		sys.stdout.write(part)
 
