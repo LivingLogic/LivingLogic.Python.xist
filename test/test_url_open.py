af3c23773781aedf4d6bfc77dbbf0eb0b4c5c90c
Walter Doerwald <walter@livinglogic.de>
2009-07-01 17:47:41 +0200

    Fix test for identity/ssh_config in ssh URLs.

--- test/test_url_open.py
+++ test/test_url_open.py
@@ -484,9 +484,9 @@ def test_walk():
 	yield check, "ll/xist/ns/", "ssh://livpython@www.livinglogic.de/~/checkouts/LivingLogic.Python.xist/src/", False
 
 
-def test_ssh_identity():
-	identity = os.path.expanduser("~/.ssh/id_rsa")
-	if not os.path.exists(identity):
-		identity = os.path.expanduser("~/.ssh/id_dsa")
+def test_ssh_config():
+	ssh_config = "/private/etc/ssh_config"
+	if not os.path.exists(ssh_config):
+		ssh_config = "/etc/ssh_config"
 	with context:
-		assert url.URL("ssh://livpython@www.livinglogic.de/~/checkouts/LivingLogic.Python.xist/").isdir(identity=identity) is True
+		assert url.URL("ssh://livpython@www.livinglogic.de/~/checkouts/LivingLogic.Python.xist/").isdir(ssh_config=ssh_config) is True
