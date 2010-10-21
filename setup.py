0b7b74c5315485628ac3a3d2fc10c5b4128db830
Walter Doerwald <walter@livinglogic.de>
2010-10-21 11:43:37 +0200

    Fix variable name.

--- setup.py
+++ setup.py
@@ -217,19 +217,19 @@ else:
 	underlines = [i for (i, line) in enumerate(news) if line.startswith("---")]
 	news = news[underlines[0]-1:underlines[1]-1]
 	news = "".join(news)
-	descr = "{}\n\n\n{}".format(DESCRIPTION.strip(), news)
+	description = "{}\n\n\n{}".format(DESCRIPTION.strip(), news)
 
 	# Get rid of text roles PyPI doesn't know about
-	descr = re.subn(":[a-z]+:`([a-zA-Z0-9_.]+)`", "``\\1``", descr)[0]
+	description = re.subn(":[a-z]+:`([a-zA-Z0-9_.]+)`", "``\\1``", description)[0]
 
 	# Expand tabs (so they won't show up as 8 spaces in the Windows installer)
-	descr = descr.expandtabs(2)
+	description = description.expandtabs(2)
 
 args = dict(
 	name="ll-xist",
 	version="3.12",
 	description="Extensible HTML/XML generator, cross-platform templating language, Oracle utilities and various other tools",
-	long_description=descr,
+	long_description=description,
 	author="Walter Doerwald",
 	author_email="walter@livinglogic.de",
 	url="http://www.livinglogic.de/Python/xist/",
