e4912b41ce7fca7b9d2905f6c6b487a03a6ab775
Walter Doerwald <walter@livinglogic.de>
2011-03-21 14:52:23 +0100

    Fix call to dt2xsc.urls2xnd().

--- src/ll/xist/scripts/dtd2xsc.py
+++ src/ll/xist/scripts/dtd2xsc.py
@@ -215,7 +215,7 @@ def main(args=None):
 	p.add_argument(      "--duplicates", dest="duplicates", help="How to handle duplicate elements from multiple DTDs (default: %(default)s)", choices=("reject", "allow", "merge"), default="reject")
 
 	args = p.parse_args(args)
-	print urls2xnd(args.urls, **args.__dict__)
+	print urls2xnd(**args.__dict__)
 
 
 if __name__ == "__main__":
