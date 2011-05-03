e9658f86f7d4f02e95f35a6b29181102f20adf8e
Walter Doerwald <walter@livinglogic.de>
2011-05-03 17:54:50 +0200

    Fix owner check for indexes.

--- src/ll/orasql/__init__.py
+++ src/ll/orasql/__init__.py
@@ -1612,7 +1612,7 @@ class Index(MixinNormalDates, Object):
 		(connection, cursor) = self.getcursor(connection)
 		if self.isconstraint(connection):
 			return ""
-		cursor.execute("select index_name, table_name, uniqueness from {}_indexes where table_owner=nvl(:owner, user) and index_name=:name".format(cursor.ddprefix()), owner=self.owner, name=self.name)
+		cursor.execute("select index_name, table_name, uniqueness from {}_indexes where owner=nvl(:owner, user) and index_name=:name".format(cursor.ddprefix()), owner=self.owner, name=self.name)
 		rec = cursor.fetchone()
 		if rec is None:
 			raise SQLObjectNotFoundError(self)
@@ -1622,7 +1622,7 @@ class Index(MixinNormalDates, Object):
 			unique = u" unique"
 		else:
 			unique = u""
-		cursor.execute("select aie.column_expression, aic.column_name from {0}_ind_columns aic, {0}_ind_expressions aie where aic.table_owner=aie.table_owner(+) and aic.index_name=aie.index_name(+) and aic.column_position=aie.column_position(+) and aic.table_owner=nvl(:owner, user) and aic.index_name=:name order by aic.column_position".format(cursor.ddprefix()), owner=self.owner, name=self.name)
+		cursor.execute("select aie.column_expression, aic.column_name from {0}_ind_columns aic, {0}_ind_expressions aie where aic.index_owner=aie.index_owner(+) and aic.index_name=aie.index_name(+) and aic.column_position=aie.column_position(+) and aic.index_owner=nvl(:owner, user) and aic.index_name=:name order by aic.column_position".format(cursor.ddprefix()), owner=self.owner, name=self.name)
 		code = u"create{} index {} on {} ({})".format(unique, indexname, tablename, ", ".join(r.column_expression or r.column_name for r in cursor))
 		if term:
 			code += u";\n"
