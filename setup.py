3d4d157b72a7edad5bad019bd3c9365ef24790f2
Walter Doerwald <walter@livinglogic.de>
2013-01-12 10:58:38 +0100

    Declare that we need Python 3.3

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ License :: OSI Approved :: MIT License
 Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
 Topic :: Software Development :: Libraries :: Python Modules
 
 # ansistyle
