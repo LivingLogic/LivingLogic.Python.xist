5bd34661206412b084f123ed6467991f42f5f7bd
Walter Doerwald <walter@livinglogic.de>
2013-01-15 13:08:11 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -240,7 +240,7 @@ else:
 
 args = dict(
 	name="ll-xist",
-	version="4.7",
+	version="4.8",
 	description="Extensible HTML/XML generator, cross-platform templating language, Oracle utilities and various other tools",
 	long_description=description,
 	author="Walter Doerwald",
