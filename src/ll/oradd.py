e62af122ca67f388677b4cae2f0c82d5f1c8286e
Walter Doerwald <walter@livinglogic.de>
2013-07-16 18:13:19 +0200

    Use tempfile.mkstemp().

--- src/ll/oradd.py
+++ src/ll/oradd.py
@@ -381,10 +381,10 @@ def importrecord(record, cursor, allkeys):
 
 
 def copyfile(name, content, allkeys):
-	with tempfile.NamedTemporaryFile(delete=False) as f:
-		f.write(content)
-		tempname = f.name
+	(f, tempname) = tempfile.mkstemp()
 	try:
+		os.write(f, content)
+		os.close(f)
 		name = name.format(**allkeys)
 		return subprocess.call("scp -q {} {}".format(tempname, name, shell=True))
 	finally:
