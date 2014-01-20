20ce1f7d65523c12e5a313b39860fb1bab96e850
Walter Doerwald <walter@livinglogic.de>
2014-01-20 18:11:33 +0100

    Reactivate tests.

--- test/test_ul4.py
+++ test/test_ul4.py
@@ -394,20 +394,20 @@ all_renderers = dict(
 	python=render_python,
 	python_dumps=render_python_dumps,
 	python_dump=render_python_dump,
-	# js=render_js,
+	js=render_js,
 	# php=render_php,
-	# java_interpreted_by_python=render_java_interpretedtemplate_by_python,
-	# java_interpreted_by_java=render_java_interpretedtemplate_by_java,
+	java_interpreted_by_python=render_java_interpretedtemplate_by_python,
+	java_interpreted_by_java=render_java_interpretedtemplate_by_java,
 )
 
 all_callers = dict(
 	python=call_python,
 	python_dumps=call_python_dumps,
 	python_dump=call_python_dump,
-	# js=call_js,
+	js=call_js,
 	# php=call_php,
-	# java_interpreted_by_python=call_java_interpretedtemplate_by_python,
-	# java_interpreted_by_java=call_java_interpretedtemplate_by_java,
+	java_interpreted_by_python=call_java_interpretedtemplate_by_python,
+	java_interpreted_by_java=call_java_interpretedtemplate_by_java,
 )
 
 
