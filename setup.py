b1099928d6da898f374ea7d558b50d4a2e650f9f
Walter Doerwald <walter@livinglogic.de>
2014-05-08 16:54:09 +0200

    Add Python 3.4 to the classifiers.

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
 Topic :: Software Development :: Libraries :: Python Modules
 
 # ansistyle
