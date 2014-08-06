16e6cf056ad9535faf4ea1620418103c81a8d3b9
Walter Doerwald <walter@livinglogic.de>
2014-08-06 15:10:58 +0200

    New features for db2ul4.
    Additional variables can be passed via the -D/--define option (this include
    database connections).
    
    Access to the oracle, sqlite, mysql and system object can be removed via the
    --oracle, --sqlite, --mysql and --system options.

--- src/ll/scripts/db2ul4.py
+++ src/ll/scripts/db2ul4.py
@@ -3,8 +3,9 @@
 # cython: language_level=3, always_allow_keywords=True
 
 """
-``db2ul4`` is a script that can be used for rendering database content into
-a UL4 template.
+``db2ul4`` is a script that can be used for rendering UL4 templates. Templates
+have access to Oracle, MySQL and SQLite databases and can execute system
+commands.
 
 
 Options
@@ -20,17 +21,74 @@ Options
 		of the files (i.e. ``foo.ul4`` will be ``templates.foo``; stdin will be
 		``templates.stdin``).
 
+	``--oracle`` : ``false``, ``no``, ``0``, ``true``, ``yes`` or ``1``
+		Provide the object ``oracle`` to the template or not (see below)?
+
+	``--sqlite`` : ``false``, ``no``, ``0``, ``true``, ``yes`` or ``1``
+		Provide the object ``sqlite`` to the template or not (see below)?
+
+	``--mysql`` : ``false``, ``no``, ``0``, ``true``, ``yes`` or ``1``
+		Provide the object ``mysql`` to the template or not (see below)?
+
+	``--system`` : ``false``, ``no``, ``0``, ``true``, ``yes`` or ``1``
+		Provide the object ``system`` to the template or not (see below)?
+
 	``-e``, ``--encoding``
 		The encoding of the templates files (default ``utf-8``)
 
 	``-w``, ``--keepws`` : ``false``, ``no``, ``0``, ``true``, ``yes`` or ``1``
-		Should line feeds and indentation be removed from the templates?
+		Should line feeds and indentation be kept in the templates or not?
+
+	``-D``, ``--define``
+		Defines an additional value that will be available inside the template.
+		``-D`` can be specified multiple times. The following formats are supported:
+
+			``var``
+				Defines ``var`` as an empty string
+
+			``var=value``
+				Defines ``var`` as the string ``value``
+
+			``var:type``
+				Defines ``var`` as an empty variable of the type ``type``
+
+			``var:type=value``
+				Defines ``var`` as a variable of the type ``type`` with the value
+				``value``
+
+		``type`` can be any of the following:
+
+			``int``
+				``value`` is an integer value.
+
+			``float``
+				``value`` is a float value.
+
+			``bool``
+				``value`` is a boolean value. ``0`` ``no``, ``false``, ``False`` or
+				the empty string will be recognized as false and ``1``, ``yes``,
+				``true`` or ``True`` will be recognized as true.
+
+			``str``
+				``value`` is string.
+
+			``oracle``
+				``value`` will be a connection to an Oracle database, e.g.::
+
+					-Ddb:oracle=user/pwd@database
+
+			``sqlite``
+				``value`` is a connection to an SQLite database.
+
+			``mysql``
+				``value`` is a connection to a MySQL database.
 
 
 Template variables
 ------------------
 
-Inside the template the following variables are available:
+Inside the template the following variables are available (if enabled via
+the matching options):
 
 	``templates``
 		A dictionary containing all the templates specified on the command line.
@@ -63,11 +121,17 @@ Inside the template the following variables are available:
 All connection objects have a ``query`` method that executes the query passed in
 and returns an iterator over the resulting records. This ``query`` method
 requires at least one positional argument. Arguments alternate between fragments
-of the SQL query and parameters that will be embedded in the query.
+of the SQL query and parameters that will be embedded in the query. For example::
+
+	<?code db = oracle.connect("user/pwd@db")?>
+	<?for p in db.query("select * from person where firstname=", name, " or lastname=", name)?>
+		...
 
 The records returned from ``query`` are dict-like objects mapping field names to
 field values.
 
+Furthermore all variables defined via the :option:`-D`/:option:`--define` option
+will be available. (Note that you can't overwrite any of the predefined variables).
 
 Example
 -------
@@ -86,8 +150,8 @@ Suppose we have a database table that looks like this::
 
 Then we can use the following template to output the table into an XML file::
 
-	<?xml version='1.0' encoding='<?print encoding?>'?>
-	<?code db = oracle.connect("user/pwd@db")?>
+	<?xml version='1.0' encoding='utf-8'?>
+	<?code db = oracle.connect("user/pwd@database')?>
 	<persons>
 		<?for p in db.query("select id, firstname, lastname from person order by 2, 1")?>
 			<person id="<?printx p.id?>">
@@ -100,7 +164,15 @@ Then we can use the following template to output the table into an XML file::
 If we put the template into the file ``person.ul4`` we can call ``db2ul4`` like
 this::
 
-	db2ul4 -o=utf-8 person.ul4 >person.xml
+	db2ul4 person.ul4 >person.xml
+
+We could also pass the connection to our database via the ``-D`` option and
+disallow the script to make any database connections or execute any system
+commands::
+
+	db2ul4 person.ul4 -Ddb:oracle=user/pwd@database --oracle=0 --sqlite=0 --mysql=0 --system=0 >person.xml
+
+Then the template could use the Oracle connection object ``db`` directly.
 """
 
 
@@ -182,11 +254,53 @@ def fixname(name):
 	return newname
 
 
+def define(arg):
+	(name, _, value) = arg.partition("=")
+	(name, _, type) = name.partition(":")
+	if any(c != "_" and not (c.isalnum() if i else c.isalpha()) for (i, c) in enumerate(name)):
+		raise argparse.ArgumentTypeError("{!r} is not a legal variable name".format(name))
+
+	if type == "int":
+		if not value:
+			return (name, 0)
+		try:
+			return (name,  int(value))
+		except ValueError:
+			raise argparse.ArgumentTypeError("{!r} is not a legal integer value".format(value))
+	elif type == "float":
+		if not value:
+			return (name, 0.)
+		try:
+			return (name, float(value))
+		except ValueError:
+			raise argparse.ArgumentTypeError("{!r} is not a legal float value".format(value))
+	elif type == "bool":
+		if value in ("", "0", "no", "false", "False"):
+			return (name, False)
+		if value in ("1", "yes", "true", "True"):
+			return (name, True)
+		raise argparse.ArgumentTypeError("{!r} is not a legal bool value".format(value))
+	elif type == "oracle":
+		return (name, Oracle().connect(value))
+	elif type == "sqlite":
+		return (name, SQLite().connect(value))
+	elif type == "mysql":
+		return (name, MySQL().connect(value))
+	elif type and type != "str":
+		raise argparse.ArgumentTypeError("{!r} is not a legal type".format(type))
+	return (name, value)
+
+
 def main(args=None):
-	p = argparse.ArgumentParser(description="render UL4 templates containing SQL statements", epilog="For more info see http://www.livinglogic.de/Python/scripts/db2ul4.html")
+	p = argparse.ArgumentParser(description="render UL4 templates with access to Oracle, MySQL or SQLite databases", epilog="For more info see http://www.livinglogic.de/Python/scripts/db2ul4.html")
 	p.add_argument("templates", metavar="template", help="templates to be used", nargs="+")
 	p.add_argument("-e", "--encoding", dest="encoding", help="Encoding for template sources (default %(default)s)", default="utf-8", metavar="ENCODING")
 	p.add_argument("-w", "--keepws", dest="keepws", help="Keep linefeeds and indentation in template sources? (default %(default)s)", action=misc.FlagAction, default=True)
+	p.add_argument(      "--oracle", dest="oracle", help="Allow the templates to connect to Oracle databases? (default %(default)s)", action=misc.FlagAction, default=True)
+	p.add_argument(      "--sqlite", dest="sqlite", help="Allow the templates to connect to SQLite databases? (default %(default)s)", action=misc.FlagAction, default=True)
+	p.add_argument(      "--mysql", dest="mysql", help="Allow the templates to connect to MySQL databases? (default %(default)s)", action=misc.FlagAction, default=True)
+	p.add_argument(      "--system", dest="system", help="Allow the templates to execute system commands? (default %(default)s)", action=misc.FlagAction, default=True)
+	p.add_argument("-D", "--define", dest="defines", metavar="var=value", help="Pass additional parameters to the template (can be specified multiple times).", action="append", type=define)
 
 	args = p.parse_args(args)
 
@@ -207,14 +321,16 @@ def main(args=None):
 			maintemplate = template
 		templates[templatename] = template
 
-	vars = dict(
-		oracle=Oracle(),
-		sqlite=SQLite(),
-		mysql=MySQL(),
-		system=System(),
-		templates=templates,
-		encoding=sys.stdout.encoding,
-	)
+	vars = dict(templates=templates, encoding=sys.stdout.encoding)
+	vars.update(args.defines)
+	if args.oracle:
+		vars["oracle"] = Oracle()
+	if args.mysql:
+		vars["mysql"] = MySQL()
+	if args.sqlite:
+		vars["sqlite"] = SQLite()
+	if args.system:
+		vars["system"] = System()
 	for part in maintemplate.render(**vars):
 		sys.stdout.write(part)
 
