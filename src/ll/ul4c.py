4ee998fe6a1b7126f19fa632351ea8d5c63dcfb5
Walter Doerwald <walter@livinglogic.de>
2014-12-17 18:36:10 +0100

    Bump UL4 version number.

--- src/ll/ul4c.py
+++ src/ll/ul4c.py
@@ -2779,7 +2779,7 @@ class Template(Block):
 	"""
 	ul4attrs = Block.ul4attrs.union({"source", "name", "keepws", "startdelim", "enddelim", "render", "renders"})
 
-	version = "31"
+	version = "32"
 
 	def __init__(self, source=None, name=None, keepws=True, startdelim="<?", enddelim="?>", signature=None):
 		"""
