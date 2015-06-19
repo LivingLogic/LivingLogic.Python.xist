ba8e5f5f65a74114c701b2ac0b42730b4e71ef31
Walter Doerwald <walter@livinglogic.de>
2015-06-19 18:39:20 +0200

    Add a workaround for http://bugs.python.org/24474 (where an addition StopIteration exception appears in the exception chain)

--- src/ll/scripts/rul4.py
+++ src/ll/scripts/rul4.py
@@ -515,9 +515,13 @@ def define(arg):
 
 def print_exception_chain(exc):
 	chain = []
+
 	while exc is not None:
 		chain.append(exc)
 		exc = exc.__cause__ if exc.__cause__ is not None else exc.__context__
+	# Workaround for http://bugs.python.org/24474
+	if isinstance(chain[-1], StopIteration):
+		chain = chain[:-1]
 	print("UL4 traceback (most recent call last):", file=sys.stderr)
 	for (i, exc) in enumerate(chain):
 		print()
