05c6fd74ca807993c4a7a4752a3028883a62f1a8
Walter Doerwald <walter@livinglogic.de>
2017-01-08 12:59:57 +0100

    Update Sphinx options.

--- docs/conf.py
+++ docs/conf.py
@@ -204,7 +204,7 @@ html_use_smartypants = True
 html_split_index = True
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
@@ -242,7 +242,7 @@ htmlhelp_basename = 'XISTdoc'
 
 latex_elements = {
 	# The paper size ('letterpaper' or 'a4paper').
-	'papersize': 'aepaper',
+	'papersize': 'a4paper',
 
 	# The font size ('10pt', '11pt' or '12pt').
 	#'pointsize': '10pt',
