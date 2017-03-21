cab25bce5e27d02ef294721e2299c12b61e8f034
Walter Doerwald <walter@livinglogic.de>
2017-03-21 18:17:42 +0100

    Add XIST version 5.27.

--- docs/makedownloads.py
+++ docs/makedownloads.py
@@ -92,6 +92,7 @@ class Version:
 
 with url.Context():
 	versions = [
+		Version("5.27", "03/21/2017"),
 		Version("5.26.1", "03/03/2017"),
 		Version("5.26", "02/28/2017"),
 		Version("5.25.1", "02/15/2017"),
