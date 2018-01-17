cccc921a4354c16252dfb2ebba7528fc371371a0
Walter Doerwald <walter@livinglogic.de>
2018-01-17 18:21:39 +0100

    Bump version number.

--- docs/makedownloads.py
+++ docs/makedownloads.py
@@ -93,6 +93,7 @@ class Version:
 
 with url.Context():
 	versions = [
+		Version("5.30", "01/17/2018"),
 		Version("5.29", "11/29/2017"),
 		Version("5.28.2", "08/03/2017"),
 		Version("5.28.1", "08/02/2017"),
