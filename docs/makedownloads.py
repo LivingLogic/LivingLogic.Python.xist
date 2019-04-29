748c5c86dd7374d77ddf185b1a47f87690a04d6a
Walter Doerwald <walter@livinglogic.de>
2019-04-29 16:29:34 +0200

    Prepare for release.

--- docs/makedownloads.py
+++ docs/makedownloads.py
@@ -93,6 +93,7 @@ class Version:
 
 with url.Context():
 	versions = [
+		Version("5.42.1", "04/29/2019"),
 		Version("5.42", "04/26/2019"),
 		Version("5.41", "03/29/2019"),
 		Version("5.40.2", "03/26/2019"),
