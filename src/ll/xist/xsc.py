15eda4e50049bbd0dadca134ae42d3e39c115734
Walter Doerwald <walter@livinglogic.de>
2021-01-11 14:04:35 +0100

    There's no :property: role in Sphinx.

--- src/ll/xist/xsc.py
+++ src/ll/xist/xsc.py
@@ -357,7 +357,7 @@ class Converter:
 	class root(misc.propclass):
 		"""
 		The root URL for the conversion. Resolving URLs during the conversion
-		process should be done relative to :property:`root`.
+		process should be done relative to :attr:`root`.
 		"""
 		def __get__(self):
 			return self.states[-1].root
