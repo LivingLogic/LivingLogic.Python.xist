b23e05ed5187e9c5cd7eeab8ffaaeb054f231b57
Walter Doerwald <walter@livinglogic.de>
2021-04-30 18:26:28 +0200

    Whitespace.

--- docs/sphinx_ll_theme/__init__.py
+++ docs/sphinx_ll_theme/__init__.py
@@ -12,11 +12,13 @@ __version_full__ = __version__
 
 
 def get_html_theme_path():
-    """Return list of HTML theme paths."""
-    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
-    return cur_dir
+	"""
+	Return list of HTML theme paths.
+	"""
+	cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+	return cur_dir
 
 
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
 def setup(app):
-    app.add_html_theme('sphinx_ll_theme', path.abspath(path.dirname(__file__)))
+	app.add_html_theme('sphinx_ll_theme', path.abspath(path.dirname(__file__)))
