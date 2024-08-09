62dde65c20e5f72d178c8e750659715742774c1f
Walter Doerwald <walter@livinglogic.de>
2024-08-09 10:53:04 +0200

    Update format of `source_suffix` to silence warning from current Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -60,12 +60,10 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {".rst": "restructuredtext"}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
