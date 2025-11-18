82607df77dab0c62fb53b554e8dfd27b4c3f4fd5
Walter Doerwald <walter@livinglogic.de>
2025-11-18 18:07:05 +0100

    Change vSQL comparison operators `<`, `<=`, `>` and `>=` to return `null` when comparing `null` with not-`null` values.

--- test/test_vsql_cmp_le.py
+++ test/test_vsql_cmp_le.py
@@ -16,11 +16,11 @@ def test_bool_none1(db, vsql_data):
 
 
 def test_bool_none2(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'") is None
 
 
 def test_bool_none3(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'") is None
 
 
 def test_int_none1(db, vsql_data):
@@ -28,7 +28,7 @@ def test_int_none1(db, vsql_data):
 
 
 def test_int_none2(db, vsql_data):
-	assert not expr(db, "r.v_int <= None", where="r.identifier == 'int'")
+	assert expr(db, "r.v_int <= None", where="r.identifier == 'int'") is None
 
 
 def test_number_none1(db, vsql_data):
@@ -36,7 +36,7 @@ def test_number_none1(db, vsql_data):
 
 
 def test_number_none2(db, vsql_data):
-	assert not expr(db, "r.v_number <= None", where="r.identifier == 'number'")
+	assert expr(db, "r.v_number <= None", where="r.identifier == 'number'") is None
 
 
 def test_str_none1(db, vsql_data):
@@ -44,7 +44,7 @@ def test_str_none1(db, vsql_data):
 
 
 def test_str_none2(db, vsql_data):
-	assert not expr(db, "r.v_str <= None", where="r.identifier == 'str'")
+	assert expr(db, "r.v_str <= None", where="r.identifier == 'str'") is None
 
 
 def test_date_none1(db, vsql_data):
@@ -52,7 +52,7 @@ def test_date_none1(db, vsql_data):
 
 
 def test_date_none2(db, vsql_data):
-	assert not expr(db, "r.v_date <= None", where="r.identifier == 'date'")
+	assert expr(db, "r.v_date <= None", where="r.identifier == 'date'") is None
 
 
 def test_datetime_none1(db, vsql_data):
@@ -60,7 +60,7 @@ def test_datetime_none1(db, vsql_data):
 
 
 def test_datetime_none2(db, vsql_data):
-	assert not expr(db, "r.v_datetime <= None", where="r.identifier == 'datetime'")
+	assert expr(db, "r.v_datetime <= None", where="r.identifier == 'datetime'") is None
 
 
 def test_color_none1(db, vsql_data):
@@ -68,7 +68,7 @@ def test_color_none1(db, vsql_data):
 
 
 def test_color_none2(db, vsql_data):
-	assert not expr(db, "r.v_color <= None", where="r.identifier == 'color'")
+	assert expr(db, "r.v_color <= None", where="r.identifier == 'color'") is None
 
 
 def test_datedelta_none1(db, vsql_data):
@@ -76,7 +76,7 @@ def test_datedelta_none1(db, vsql_data):
 
 
 def test_datedelta_none2(db, vsql_data):
-	assert not expr(db, "r.v_datedelta <= None", where="r.identifier == 'datedelta'")
+	assert expr(db, "r.v_datedelta <= None", where="r.identifier == 'datedelta'") is None
 
 
 def test_datetimedelta_none1(db, vsql_data):
@@ -84,7 +84,7 @@ def test_datetimedelta_none1(db, vsql_data):
 
 
 def test_datetimedelta_none2(db, vsql_data):
-	assert not expr(db, "r.v_datetimedelta <= None", where="r.identifier == 'datetimedelta'")
+	assert expr(db, "r.v_datetimedelta <= None", where="r.identifier == 'datetimedelta'") is None
 
 
 def test_monthdelta_none1(db, vsql_data):
@@ -92,27 +92,27 @@ def test_monthdelta_none1(db, vsql_data):
 
 
 def test_monthdelta_none2(db, vsql_data):
-	assert not expr(db, "r.v_monthdelta <= None", where="r.identifier == 'monthdelta'")
+	assert expr(db, "r.v_monthdelta <= None", where="r.identifier == 'monthdelta'") is None
 
 
 def test_intlist_none(db, vsql_data):
-	assert not expr(db, "[1, 2] <= None")
+	assert expr(db, "[1, 2] <= None") is None
 
 
 def test_numberlist_none(db, vsql_data):
-	assert not expr(db, "[1.2, 3.4] <= None")
+	assert expr(db, "[1.2, 3.4] <= None") is None
 
 
 def test_strlist_none(db, vsql_data):
-	assert not expr(db, "['foo', 'bar'] <= None")
+	assert expr(db, "['foo', 'bar'] <= None") is None
 
 
 def test_datelist_none(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29)] <= None")
+	assert expr(db, "[@(2000-02-29)] <= None") is None
 
 
 def test_datetimelist_none(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29T12:34:56)] <= None")
+	assert expr(db, "[@(2000-02-29T12:34:56)] <= None") is None
 
 
 def test_none_bool1(db, vsql_data):
@@ -120,11 +120,11 @@ def test_none_bool1(db, vsql_data):
 
 
 def test_none_bool2(db, vsql_data):
-	assert expr(db, "None <= r.v_bool", where="r.identifier == 'bool_false'")
+	assert expr(db, "None <= r.v_bool", where="r.identifier == 'bool_false'") is None
 
 
 def test_none_bool3(db, vsql_data):
-	assert expr(db, "None <= r.v_bool", where="r.identifier == 'bool_true'")
+	assert expr(db, "None <= r.v_bool", where="r.identifier == 'bool_true'") is None
 
 
 def test_none_int1(db, vsql_data):
@@ -132,7 +132,7 @@ def test_none_int1(db, vsql_data):
 
 
 def test_none_int2(db, vsql_data):
-	assert expr(db, "None <= r.v_int", where="r.identifier == 'int'")
+	assert expr(db, "None <= r.v_int", where="r.identifier == 'int'") is None
 
 
 def test_none_number1(db, vsql_data):
@@ -140,7 +140,7 @@ def test_none_number1(db, vsql_data):
 
 
 def test_none_number2(db, vsql_data):
-	assert expr(db, "None <= r.v_number", where="r.identifier == 'number'")
+	assert expr(db, "None <= r.v_number", where="r.identifier == 'number'") is None
 
 
 def test_none_str1(db, vsql_data):
@@ -148,7 +148,7 @@ def test_none_str1(db, vsql_data):
 
 
 def test_none_str2(db, vsql_data):
-	assert expr(db, "None <= r.v_str", where="r.identifier == 'str'")
+	assert expr(db, "None <= r.v_str", where="r.identifier == 'str'") is None
 
 
 def test_none_date1(db, vsql_data):
@@ -156,7 +156,7 @@ def test_none_date1(db, vsql_data):
 
 
 def test_none_date2(db, vsql_data):
-	assert expr(db, "None <= r.v_date", where="r.identifier == 'date'")
+	assert expr(db, "None <= r.v_date", where="r.identifier == 'date'") is None
 
 
 def test_none_datetime1(db, vsql_data):
@@ -164,7 +164,7 @@ def test_none_datetime1(db, vsql_data):
 
 
 def test_none_datetime2(db, vsql_data):
-	assert expr(db, "None <= r.v_datetime", where="r.identifier == 'datetime'")
+	assert expr(db, "None <= r.v_datetime", where="r.identifier == 'datetime'") is None
 
 
 def test_none_color1(db, vsql_data):
@@ -172,7 +172,7 @@ def test_none_color1(db, vsql_data):
 
 
 def test_none_color2(db, vsql_data):
-	assert expr(db, "None <= r.v_color", where="r.identifier == 'color'")
+	assert expr(db, "None <= r.v_color", where="r.identifier == 'color'") is None
 
 
 def test_none_datedelta1(db, vsql_data):
@@ -180,7 +180,7 @@ def test_none_datedelta1(db, vsql_data):
 
 
 def test_none_datedelta2(db, vsql_data):
-	assert expr(db, "None <= r.v_datedelta", where="r.identifier == 'datedelta'")
+	assert expr(db, "None <= r.v_datedelta", where="r.identifier == 'datedelta'") is None
 
 
 def test_none_datetimedelta1(db, vsql_data):
@@ -188,7 +188,7 @@ def test_none_datetimedelta1(db, vsql_data):
 
 
 def test_none_datetimedelta2(db, vsql_data):
-	assert expr(db, "None <= r.v_datetimedelta", where="r.identifier == 'datetimedelta'")
+	assert expr(db, "None <= r.v_datetimedelta", where="r.identifier == 'datetimedelta'") is None
 
 
 def test_none_monthdelta1(db, vsql_data):
@@ -196,27 +196,27 @@ def test_none_monthdelta1(db, vsql_data):
 
 
 def test_none_monthdelta2(db, vsql_data):
-	assert expr(db, "None <= r.v_monthdelta", where="r.identifier == 'monthdelta'")
+	assert expr(db, "None <= r.v_monthdelta", where="r.identifier == 'monthdelta'") is None
 
 
 def test_none_intlist(db, vsql_data):
-	assert expr(db, "None <= [1, 2]")
+	assert expr(db, "None <= [1, 2]") is None
 
 
 def test_none_numberlist(db, vsql_data):
-	assert expr(db, "None <= [1.2, 3.4]")
+	assert expr(db, "None <= [1.2, 3.4]") is None
 
 
 def test_none_strlist(db, vsql_data):
-	assert expr(db, "None <= ['foo', 'bar']")
+	assert expr(db, "None <= ['foo', 'bar']") is None
 
 
 def test_none_datelist(db, vsql_data):
-	assert expr(db, "None <= [@(2000-02-29)]")
+	assert expr(db, "None <= [@(2000-02-29)]") is None
 
 
 def test_none_datetimelist(db, vsql_data):
-	assert expr(db, "None <= [@(2000-02-29T12:34:56)]")
+	assert expr(db, "None <= [@(2000-02-29T12:34:56)]") is None
 
 
 def test_bool_bool1(db, vsql_data):
@@ -224,15 +224,15 @@ def test_bool_bool1(db, vsql_data):
 
 
 def test_bool_bool2(db, vsql_data):
-	assert expr(db, "r.v_bool <= False", where="r.identifier == 'none'")
+	assert expr(db, "r.v_bool <= False", where="r.identifier == 'none'") is None
 
 
 def test_bool_bool3(db, vsql_data):
-	assert expr(db, "r.v_bool <= True", where="r.identifier == 'none'")
+	assert expr(db, "r.v_bool <= True", where="r.identifier == 'none'") is None
 
 
 def test_bool_bool4(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'") is None
 
 
 def test_bool_bool5(db, vsql_data):
@@ -244,7 +244,7 @@ def test_bool_bool6(db, vsql_data):
 
 
 def test_bool_bool7(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'") is None
 
 
 def test_bool_bool8(db, vsql_data):
@@ -260,11 +260,11 @@ def test_bool_int1(db, vsql_data):
 
 
 def test_bool_int2(db, vsql_data):
-	assert expr(db, "r.v_bool <= -1", where="r.identifier == 'none'")
+	assert expr(db, "r.v_bool <= -1", where="r.identifier == 'none'") is None
 
 
 def test_bool_int3(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'") is None
 
 
 def test_bool_int4(db, vsql_data):
@@ -276,7 +276,7 @@ def test_bool_int5(db, vsql_data):
 
 
 def test_bool_int6(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'") is None
 
 
 def test_bool_int7(db, vsql_data):
@@ -292,11 +292,11 @@ def test_bool_number1(db, vsql_data):
 
 
 def test_bool_number2(db, vsql_data):
-	assert expr(db, "r.v_bool <= -1.0", where="r.identifier == 'none'")
+	assert expr(db, "r.v_bool <= -1.0", where="r.identifier == 'none'") is None
 
 
 def test_bool_number3(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_false'") is None
 
 
 def test_bool_number4(db, vsql_data):
@@ -308,7 +308,7 @@ def test_bool_number5(db, vsql_data):
 
 
 def test_bool_number6(db, vsql_data):
-	assert not expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'")
+	assert expr(db, "r.v_bool <= None", where="r.identifier == 'bool_true'") is None
 
 
 def test_bool_number7(db, vsql_data):
@@ -324,15 +324,15 @@ def test_int_bool1(db, vsql_data):
 
 
 def test_int_bool2(db, vsql_data):
-	assert expr(db, "r.v_int <= False", where="r.identifier == 'none'")
+	assert expr(db, "r.v_int <= False", where="r.identifier == 'none'") is None
 
 
 def test_int_bool3(db, vsql_data):
-	assert expr(db, "r.v_int <= True", where="r.identifier == 'none'")
+	assert expr(db, "r.v_int <= True", where="r.identifier == 'none'") is None
 
 
 def test_int_bool4(db, vsql_data):
-	assert not expr(db, "r.v_int <= None", where="r.identifier == 'int'")
+	assert expr(db, "r.v_int <= None", where="r.identifier == 'int'") is None
 
 
 def test_int_bool5(db, vsql_data):
@@ -344,7 +344,7 @@ def test_int_bool6(db, vsql_data):
 
 
 def test_int_bool7(db, vsql_data):
-	assert not expr(db, "-r.v_int <= None", where="r.identifier == 'int'")
+	assert expr(db, "-r.v_int <= None", where="r.identifier == 'int'") is None
 
 
 def test_int_bool8(db, vsql_data):
@@ -360,7 +360,7 @@ def test_int_int1(db, vsql_data):
 
 
 def test_int_int2(db, vsql_data):
-	assert expr(db, "r.v_int <= 1", where="r.identifier == 'none'")
+	assert expr(db, "r.v_int <= 1", where="r.identifier == 'none'") is None
 
 
 def test_int_int3(db, vsql_data):
@@ -372,7 +372,7 @@ def test_int_int4(db, vsql_data):
 
 
 def test_int_int5(db, vsql_data):
-	assert not expr(db, "1776 <= r.v_int", where="r.identifier == 'none'")
+	assert expr(db, "1776 <= r.v_int", where="r.identifier == 'none'") is None
 
 
 def test_number_bool1(db, vsql_data):
@@ -380,7 +380,7 @@ def test_number_bool1(db, vsql_data):
 
 
 def test_number_bool2(db, vsql_data):
-	assert expr(db, "r.v_number <= False", where="r.identifier == 'none'")
+	assert expr(db, "r.v_number <= False", where="r.identifier == 'none'") is None
 
 
 def test_number_bool3(db, vsql_data):
@@ -396,7 +396,7 @@ def test_number_int1(db, vsql_data):
 
 
 def test_number_int2(db, vsql_data):
-	assert expr(db, "r.v_number <= 1", where="r.identifier == 'none'")
+	assert expr(db, "r.v_number <= 1", where="r.identifier == 'none'") is None
 
 
 def test_number_int3(db, vsql_data):
@@ -412,7 +412,7 @@ def test_number_number1(db, vsql_data):
 
 
 def test_number_number2(db, vsql_data):
-	assert expr(db, "r.v_number <= 1.0", where="r.identifier == 'none'")
+	assert expr(db, "r.v_number <= 1.0", where="r.identifier == 'none'") is None
 
 
 def test_number_number3(db, vsql_data):
@@ -552,11 +552,11 @@ def test_nulllist_intlist1(db, vsql_data):
 
 
 def test_nulllist_intlist2(db, vsql_data):
-	assert expr(db, "[None] <= [1]")
+	assert expr(db, "[None] <= [1]") is None
 
 
 def test_nulllist_intlist3(db, vsql_data):
-	assert expr(db, "[None, None] <= [1]")
+	assert expr(db, "[None, None] <= [1]") is None
 
 
 def test_nulllist_numberlist1(db, vsql_data):
@@ -564,11 +564,11 @@ def test_nulllist_numberlist1(db, vsql_data):
 
 
 def test_nulllist_numberlist2(db, vsql_data):
-	assert expr(db, "[None] <= [1.1]")
+	assert expr(db, "[None] <= [1.1]") is None
 
 
 def test_nulllist_numberlist3(db, vsql_data):
-	assert expr(db, "[None, None] <= [1.1]")
+	assert expr(db, "[None, None] <= [1.1]") is None
 
 
 def test_nulllist_strlist1(db, vsql_data):
@@ -576,11 +576,11 @@ def test_nulllist_strlist1(db, vsql_data):
 
 
 def test_nulllist_strlist2(db, vsql_data):
-	assert expr(db, "[None] <= ['gurk']")
+	assert expr(db, "[None] <= ['gurk']") is None
 
 
 def test_nulllist_strlist3(db, vsql_data):
-	assert expr(db, "[None, None] <= ['gurk']")
+	assert expr(db, "[None, None] <= ['gurk']") is None
 
 
 def test_nulllist_datelist1(db, vsql_data):
@@ -588,11 +588,11 @@ def test_nulllist_datelist1(db, vsql_data):
 
 
 def test_nulllist_datelist2(db, vsql_data):
-	assert expr(db, "[None] <= [@(2000-02-29)]")
+	assert expr(db, "[None] <= [@(2000-02-29)]") is None
 
 
 def test_nulllist_datelist3(db, vsql_data):
-	assert expr(db, "[None, None] <= [@(2000-02-29)]")
+	assert expr(db, "[None, None] <= [@(2000-02-29)]") is None
 
 
 def test_nulllist_datetimelist1(db, vsql_data):
@@ -600,11 +600,11 @@ def test_nulllist_datetimelist1(db, vsql_data):
 
 
 def test_nulllist_datetimelist2(db, vsql_data):
-	assert expr(db, "[None] <= [@(2000-02-29T12:34:56)]")
+	assert expr(db, "[None] <= [@(2000-02-29T12:34:56)]") is None
 
 
 def test_nulllist_datetimelist3(db, vsql_data):
-	assert expr(db, "[None, None] <= [@(2000-02-29T12:34:56)]")
+	assert expr(db, "[None, None] <= [@(2000-02-29T12:34:56)]") is None
 
 
 def test_intlist_nulllist1(db, vsql_data):
@@ -612,11 +612,11 @@ def test_intlist_nulllist1(db, vsql_data):
 
 
 def test_intlist_nulllist2(db, vsql_data):
-	assert not expr(db, "[1] <= [None]")
+	assert expr(db, "[1] <= [None]") is None
 
 
 def test_intlist_nulllist3(db, vsql_data):
-	assert not expr(db, "[1] <= [None, None]")
+	assert expr(db, "[1] <= [None, None]") is None
 
 
 def test_numberlist_nulllist1(db, vsql_data):
@@ -624,11 +624,11 @@ def test_numberlist_nulllist1(db, vsql_data):
 
 
 def test_numberlist_nulllist2(db, vsql_data):
-	assert not expr(db, "[1.1] <= [None]")
+	assert expr(db, "[1.1] <= [None]") is None
 
 
 def test_numberlist_nulllist3(db, vsql_data):
-	assert not expr(db, "[1.1] <= [None, None]")
+	assert expr(db, "[1.1] <= [None, None]") is None
 
 
 def test_strlist_nulllist1(db, vsql_data):
@@ -640,7 +640,7 @@ def test_strlist_nulllist2(db, vsql_data):
 
 
 def test_strlist_nulllist3(db, vsql_data):
-	assert not expr(db, "['gurk'] <= [None, None]")
+	assert expr(db, "['gurk'] <= [None, None]") is None
 
 
 def test_datelist_nulllist1(db, vsql_data):
@@ -648,11 +648,11 @@ def test_datelist_nulllist1(db, vsql_data):
 
 
 def test_datelist_nulllist2(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29)] <= [None]")
+	assert expr(db, "[@(2000-02-29)] <= [None]") is None
 
 
 def test_datelist_nulllist3(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29)] <= [None, None]")
+	assert expr(db, "[@(2000-02-29)] <= [None, None]") is None
 
 
 def test_datetimelist_nulllist1(db, vsql_data):
@@ -660,8 +660,8 @@ def test_datetimelist_nulllist1(db, vsql_data):
 
 
 def test_datetimelist_nulllist2(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29T12:34:56)] <= [None]")
+	assert expr(db, "[@(2000-02-29T12:34:56)] <= [None]") is None
 
 
 def test_datetimelist_nulllist3(db, vsql_data):
-	assert not expr(db, "[@(2000-02-29T12:34:56)] <= [None, None]")
+	assert expr(db, "[@(2000-02-29T12:34:56)] <= [None, None]") is None
