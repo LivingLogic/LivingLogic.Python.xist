280d80b3c9eb6213b7a96c0fa4cbfdb5a5d00a0a
Walter Doerwald <walter@livinglogic.de>
2025-11-20 12:49:35 +0100

    Bump Python version number fir intersphinx links.

--- docs/conf.py
+++ docs/conf.py
@@ -377,7 +377,7 @@ texinfo_documents = [
 
 # Configuration for intersphinx: refer to other Python libraries.
 intersphinx_mapping = {
-	"python": ("https://docs.python.org/3.8", None),
+	"python": ("https://docs.python.org/3.14", None),
 	"requests": ("https://requests.readthedocs.io/en/latest", None),
 	"oracledb": ("https://python-oracledb.readthedocs.io/en/latest", None),
 	"cx_Oracle": ("https://cx-oracle.readthedocs.io/en/latest", None),
