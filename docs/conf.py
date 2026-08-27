ae94e778640d12808bb16bd2cdee2930413ec621
Walter Doerwald <walter@livinglogic.de>
2026-08-27 13:55:58 +0200

    Update to `sphinx-linklint` (which is the new version of `linklint`).
    This fixes https://github.com/LivingLogic/LivingLogic.Python.xist/issues/3 (reported by Ned Batchelder)

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ extensions = [
 	"sphinx.ext.intersphinx",
 	"sphinx.ext.viewcode",
 	"sphinxcontrib.jquery",
-	"linklint.ext",
+	"sphinx_linklint.ext",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
